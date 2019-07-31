131d32bc347187edaddaa312e914f8295a770b2e
Drillgon200 <50186362+Drillgon200@users.noreply.github.com>
2019-07-30 22:20:16 -0700

    Fixed siren bug

--- com/hbm/packet/TESirenPacket.java
+++ com/hbm/packet/TESirenPacket.java
@@ -22,8 +22,11 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
 
 public class TESirenPacket implements IMessage {
 
@@ -32,19 +35,23 @@ public class TESirenPacket implements IMessage {
 	int z;
 	int id;
 	boolean active;
+	WorldServer world;
+	int worldId;
 
 	public TESirenPacket()
 	{
 		
 	}
 
-	public TESirenPacket(int x, int y, int z, int id, boolean active)
+	public TESirenPacket(int x, int y, int z, int id, boolean active, WorldServer world)
 	{
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.id = id;
 		this.active = active;
+		this.world = world;
+		this.worldId = this.getWorldIdInArray();
 	}
 
 	@Override
@@ -54,6 +61,7 @@ public class TESirenPacket implements IMessage {
 		z = buf.readInt();
 		id = buf.readInt();
 		active = buf.readBoolean();
+		worldId = buf.readInt();
 	}
 
 	@Override
@@ -63,6 +71,19 @@ public class TESirenPacket implements IMessage {
 		buf.writeInt(z);
 		buf.writeInt(id);
 		buf.writeBoolean(active);
+		buf.writeInt(worldId);
+	}
+
+	public int getWorldIdInArray() {
+		for(int i = 0; i < MinecraftServer.getServer().worldServers.length; i++){
+
+			if(MinecraftServer.getServer().worldServers[i] == this.world){
+
+				return i;
+			}
+			
+		}
+	return -1;
 	}
 
 	public static class Handler implements IMessageHandler<TESirenPacket, IMessage> {
@@ -70,23 +91,31 @@ public class TESirenPacket implements IMessage {
 		@Override
 		@SideOnly(Side.CLIENT)
 		public IMessage onMessage(TESirenPacket m, MessageContext ctx) {
-			TileEntity te = Minecraft.getMinecraft().theWorld.getTileEntity(m.x, m.y, m.z);
-
-			if (te != null && te instanceof TileEntityMachineSiren) {
-				
+			//TileEntity te = Minecraft.getMinecraft().theWorld.getTileEntity(m.x, m.y, m.z);
+			TileEntity te2 = null;
+			if(m.worldId != -1){
+			te2 = MinecraftServer.getServer().worldServers[m.worldId].getTileEntity(m.x, m.y, m.z);
+			}
+	
+			
+			
+			if (te2 != null && te2 instanceof TileEntityMachineSiren) {
 				SoundLoopSiren sound = null;
+	
 				for(int i = 0; i < SoundLoopSiren.list.size(); i++)  {
-					if(SoundLoopSiren.list.get(i).getTE() == te)
+					if(SoundLoopSiren.list.get(i).getTE() == te2)
 						sound = SoundLoopSiren.list.get(i);
 				}
 				
 				if(m.active) {
 					
 					if(sound == null) {
+
 						//Start sound
+						
 						if(m.id > 0) {
 							boolean b = TrackType.getEnum(m.id).getType().name().equals(SoundType.LOOP.name());
-							SoundLoopSiren s = new SoundLoopSiren(TrackType.getEnum(m.id).getSoundLocation(), te, TrackType.getEnum(m.id).getType());
+							SoundLoopSiren s = new SoundLoopSiren(TrackType.getEnum(m.id).getSoundLocation(), te2, TrackType.getEnum(m.id).getType());
 							s.setRepeat(b);
 							s.intendedVolume = TrackType.getEnum(m.id).getVolume();
 							Minecraft.getMinecraft().getSoundHandler().playSound(s);
@@ -99,9 +128,11 @@ public class TESirenPacket implements IMessage {
 						
 							if(!sound.getPath().equals(path)) {
 								//Track switched, stop and restart
+
+							
 								sound.endSound();
 								if(m.id > 0)
-									Minecraft.getMinecraft().getSoundHandler().playSound(new SoundLoopSiren(TrackType.getEnum(m.id).getSoundLocation(), te, TrackType.getEnum(m.id).getType()));
+									Minecraft.getMinecraft().getSoundHandler().playSound(new SoundLoopSiren(TrackType.getEnum(m.id).getSoundLocation(), te2, TrackType.getEnum(m.id).getType()));
 							}
 						}
 						
