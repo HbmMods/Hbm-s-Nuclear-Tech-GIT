832660f8b0f7cff49c42e74e44d46d6c14d20149
Bob <hbmmods@gmail.com>
2021-12-09 22:51:52 +0100

    fixed iGen render jank, giant blast furnace UV

--- src/main/java/com/hbm/render/tileentity/RenderIGenerator.java
+++ src/main/java/com/hbm/render/tileentity/RenderIGenerator.java
@@ -19,8 +19,6 @@ public class RenderIGenerator extends TileEntitySpecialRenderer {
 	public void renderTileEntityAt(TileEntity te, double x, double y, double z, float f) {
 		GL11.glPushMatrix();
 		GL11.glTranslated(x + 0.5D, y, z + 0.5D);
-		
-		GL11.glTranslated(1, 0, 0);
 
 		switch(te.getBlockMetadata() - BlockDummyable.offset) {
 		case 2: GL11.glRotatef(180, 0F, 1F, 0F); break;
@@ -28,6 +26,8 @@ public class RenderIGenerator extends TileEntitySpecialRenderer {
 		case 3: GL11.glRotatef(0, 0F, 1F, 0F); break;
 		case 5: GL11.glRotatef(90, 0F, 1F, 0F); break;
 		}
+		
+		GL11.glTranslated(0, 0, -1);
 
 		TileEntityMachineIGenerator igen = (TileEntityMachineIGenerator) te;
 
