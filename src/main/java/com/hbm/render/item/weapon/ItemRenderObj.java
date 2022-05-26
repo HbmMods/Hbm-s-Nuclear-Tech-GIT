8c25e95b6dd0cfad50df0eecfa1e730664d9a5a2
umbrykid <84083187+1umbrykid@users.noreply.github.com>
2022-05-26 19:41:30 -0300

    Hi There, would you like to sign my pettion?
    You gonna be fucking kidding me.
    
    Changes:
    
    - Brimstone now have 3D Icon
    
    - Reworked the recipe (requires 2 advanced revolver mechanism and a sixflod lithum battery)

--- src/main/java/com/hbm/render/item/weapon/ItemRenderObj.java
+++ src/main/java/com/hbm/render/item/weapon/ItemRenderObj.java
@@ -11,14 +11,13 @@ import net.minecraftforge.client.IItemRenderer;
 
 public class ItemRenderObj implements IItemRenderer {
 	
-	public ItemRenderObj() { }
-
 	@Override
 	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
 		switch(type) {
 		case EQUIPPED:
 		case EQUIPPED_FIRST_PERSON:
 		case ENTITY:
+		case INVENTORY:
 			return true;
 		default: return false;
 		}
@@ -26,12 +25,27 @@ public class ItemRenderObj implements IItemRenderer {
 
 	@Override
 	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
-		return false;
+		return type == ItemRenderType.ENTITY && (helper == ItemRendererHelper.ENTITY_ROTATION || helper == ItemRendererHelper.ENTITY_BOBBING);
 	}
 
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+		GL11.glPushMatrix();
+
+		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glShadeModel(GL11.GL_SMOOTH);
+		
 		switch(type) {
+		
+		case INVENTORY:
+			GL11.glEnable(GL11.GL_LIGHTING);
+			
+			double s = 4.0D;
+			GL11.glTranslated(6, 10, 0);
+			GL11.glRotated(90, 0, 1, 0);
+			GL11.glRotated(135, 1, 0, 0);
+			GL11.glScaled(s, s, -s);
+			break;
 		case EQUIPPED_FIRST_PERSON:
 			GL11.glRotatef(70F, 0.0F, 1.0F, 0.0F);
 			GL11.glRotatef(-50F, 1.0F, 0.0F, 0.0F);
@@ -39,19 +53,19 @@ public class ItemRenderObj implements IItemRenderer {
 		case EQUIPPED:
 		case ENTITY:
 		default:
-			GL11.glPushMatrix();
-				if(item.getItem() == ModItems.gun_brimstone)
-					Minecraft.getMinecraft().renderEngine.bindTexture(ResourceManager.brimstone_tex);
-				//GL11.glRotatef(-135.0F, 0.0F, 0.0F, 1.0F);
 				GL11.glRotatef(10F, 1.0F, 0.0F, 0.0F);
 				GL11.glRotatef(190F, 0.0F, 1.0F, 0.0F);
 				GL11.glRotatef(-15F, 0.0F, 0.0F, 1.0F);
 				GL11.glScalef(0.75F, 0.75F, 0.75F);
 				GL11.glTranslatef(-0.7F, -0.4F, -1.1F);
 				GL11.glDisable(GL11.GL_CULL_FACE);
-				if(item.getItem() == ModItems.gun_brimstone)
-					ResourceManager.brimstone.renderAll();
-			GL11.glPopMatrix(); break;
+			break;
+
 		}
+			Minecraft.getMinecraft().renderEngine.bindTexture(ResourceManager.brimstone_tex);
+			ResourceManager.brimstone.renderAll();
+			
+			GL11.glShadeModel(GL11.GL_FLAT);
+			GL11.glPopMatrix();
 	}
 }
