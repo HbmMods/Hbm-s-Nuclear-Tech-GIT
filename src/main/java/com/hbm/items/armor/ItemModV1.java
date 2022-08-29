fea8dd9628e8f4c25220b1ccb40d48102243f1a6
Boblet <hbmmods@gmail.com>
2022-08-29 10:28:00 +0200

    hakita redemption arc

--- src/main/java/com/hbm/items/armor/ItemModV1.java
+++ src/main/java/com/hbm/items/armor/ItemModV1.java
@@ -7,19 +7,15 @@ import com.google.common.collect.Multimap;
 import com.hbm.handler.ArmorModHandler;
 import com.hbm.interfaces.IArmorModDash;
 
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 public class ItemModV1 extends ItemArmorMod implements IArmorModDash {
 	
 	private static final UUID speed = UUID.fromString("1d11e63e-28c4-4e14-b09f-fe0bd1be708f");
-	public static final UUID its_to_prevent_you_from_healing = UUID.fromString("9b0ff87a-6103-482b-b515-19ef2e6721ce");
 
 	public ItemModV1() {
 		super(ArmorModHandler.extra, false, true, false, false);
@@ -47,21 +43,5 @@ public class ItemModV1 extends ItemArmorMod implements IArmorModDash {
 	
 	public int getDashes() {
 		return 3;
-	}
-
-	@Override
-	public void modDamage(LivingHurtEvent event, ItemStack armor) {
-		
-		EntityLivingBase entity = event.entityLiving;
-		
-		IAttributeInstance attributeinstance = entity.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.maxHealth);
-		
-		float health = entity.getMaxHealth();
-		
-		try {
-			attributeinstance.removeModifier(attributeinstance.getModifier(its_to_prevent_you_from_healing));
-		} catch(Exception ex) { }
-		
-		attributeinstance.applyModifier(new AttributeModifier(its_to_prevent_you_from_healing, "hakita what is this bullshit", -(entity.getMaxHealth() - health + event.ammount * 0.35), 0));
 	}
 }
