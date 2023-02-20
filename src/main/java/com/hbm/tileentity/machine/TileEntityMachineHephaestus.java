bd9c6f8994e9f97af623a3c6ba02bfe7b8d808bf
JameH2 <egekarabilecen@gmail.com>
2023-02-19 17:11:41 -0700

    Update TileEntityMachineHephaestus.java

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineHephaestus.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineHephaestus.java
@@ -20,21 +20,83 @@ import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraftforge.common.util.ForgeDirection;
+
 public class TileEntityMachineHephaestus extends TileEntityLoadedBase implements INBTPacketReceiver, IFluidStandardTransceiver {
 
 	public FluidTank input;
 	public FluidTank output;
+	public int bufferedHeat;
+	
+	public float rot;
+	public float prevRot;
 
 	public TileEntityMachineHephaestus() {
 		this.input = new FluidTank(Fluids.OIL, 24_000);
 		this.output = new FluidTank(Fluids.HOTOIL, 24_000);
 	}
 	
+	private int[] heat = new int[10];
+	private long fissureScanTime;
+	
 	@Override
 	public void updateEntity() {
 
 		if(!worldObj.isRemote) {
-			this.updateConnections();
+			
+			setupTanks();
+			
+			if(worldObj.getTotalWorldTime() % 20 == 0) {
+				this.updateConnections();
+			}
+			
+			int height = (int) (worldObj.getTotalWorldTime() % 10);
+			int range = 7;
+			int y = yCoord - 1 - height;
+			
+			heat[height] = 0;
+			
+			if(y >= 0) {
+				for(int x = -range; x <= range; x++) {
+					for(int z = -range; z <= range; z++) {
+						heat[height] += heatFromBlock(xCoord + x, y, zCoord + z);
+					}
+				}
+			}
+			
+			NBTTagCompound data = new NBTTagCompound();
+			input.writeToNBT(data, "i");
+			
+			heatFluid();
+			
+			output.writeToNBT(data, "o");
+			
+			if(output.getFill() > 0) {
+				for(DirPos pos : getConPos()) {
+					this.sendFluid(output.getTankType(), worldObj, pos.getX(), pos.getY(), pos.getZ(), pos.getDir());
+				}
+			}
+			data.setInteger("heat", this.getTotalHeat());
+			INBTPacketReceiver.networkPack(this, data, 150);
+			
+		} else {
+			
+			this.prevRot = this.rot;
+			
+			if(this.bufferedHeat > 0) {
+				this.rot += 0.5F;
+				
+				if(worldObj.rand.nextInt(7) == 0) {
+					double x = worldObj.rand.nextGaussian() * 2;
+					double y = worldObj.rand.nextGaussian() * 3;
+					double z = worldObj.rand.nextGaussian() * 2;
+					worldObj.spawnParticle("cloud", xCoord + 0.5 + x, yCoord + 6 + y, zCoord + 0.5 + z, 0, 0, 0);
+				}
+			}
+			
+			if(this.rot >= 360F) {
+				this.prevRot -= 360F;
+				this.rot -= 360F;
+			}
 		}
 	}
 	
@@ -107,10 +169,15 @@ public class TileEntityMachineHephaestus extends TileEntityLoadedBase implements
 
 	@Override
 	public void networkUnpack(NBTTagCompound nbt) {
+		input.readFromNBT(nbt, "i");
+		output.readFromNBT(nbt, "o");
 		
+		this.bufferedHeat = nbt.getInteger("heat");
 	}
 	
 	private void updateConnections() {
+		
+		if(input.getTankType() == Fluids.NONE) return;
 		
 		for(DirPos pos : getConPos()) {
 			this.trySubscribe(input.getTankType(), worldObj, pos.getX(), pos.getY(), pos.getZ(), pos.getDir());
@@ -149,5 +216,30 @@ public class TileEntityMachineHephaestus extends TileEntityLoadedBase implements
 	@Override
 	public boolean canConnect(FluidType type, ForgeDirection dir) {
 		return dir != ForgeDirection.UNKNOWN && dir != ForgeDirection.UP && dir != ForgeDirection.DOWN;
+	}
+	
+	AxisAlignedBB bb = null;
+	
+	@Override
+	public AxisAlignedBB getRenderBoundingBox() {
+		
+		if(bb == null) {
+			bb = AxisAlignedBB.getBoundingBox(
+					xCoord - 3,
+					yCoord,
+					zCoord - 3,
+					xCoord + 4,
+					yCoord + 12,
+					zCoord + 4
+					);
+		}
+		
+		return bb;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public double getMaxRenderDistanceSquared() {
+		return 65536.0D;
 	}
 }
