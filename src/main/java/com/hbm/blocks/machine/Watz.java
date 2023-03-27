1cf051a8b10614b0b48471654a6d9ea821cba3b3
Bob <hbmmods@gmail.com>
2023-03-27 21:47:01 +0200

    ough

--- src/main/java/com/hbm/blocks/machine/Watz.java
+++ src/main/java/com/hbm/blocks/machine/Watz.java
@@ -25,7 +25,8 @@ public class Watz extends BlockDummyable {
 	
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-		return super.standardOpenBehavior(world, x, y, z, player, 0);
+		//return super.standardOpenBehavior(world, x, y, z, player, 0);
+		return false;
 	}
 
 	@Override
