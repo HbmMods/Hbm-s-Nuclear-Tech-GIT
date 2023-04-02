7898d47f1a5fe31788d744f492f744b6f8d349bd
JameH2 <egekarabilecen@gmail.com>
2023-04-02 13:07:04 -0600

    Update TileEntityMachineMixer.java

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineMixer.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineMixer.java
@@ -149,19 +149,22 @@ public class TileEntityMachineMixer extends TileEntityMachineBase implements INB
 			
 			if(recipe.input1.type != tanks[0].getTankType()) {
 				tanks[0].setTankType(recipe.input1.type);
-			}
-			
-			if(tanks[0].getFill() < recipe.input1.fill) return false;
-		}
-		
-		if(recipe.input2 != null) {
-			
-			if(recipe.input2.type != tanks[1].getTankType()) {
 				tanks[1].setTankType(recipe.input2.type);
 			}
 			
+			if(tanks[0].getFill() < recipe.input1.fill) return false;
 			if(tanks[1].getFill() < recipe.input2.fill) return false;
 		}
+		
+		
+		//if(recipe.input2 != null) {
+			
+			//if(recipe.input2.type != tanks[1].getTankType()) {
+				//tanks[1].setTankType(recipe.input2.type);
+			//}
+			//
+			//if(tanks[1].getFill() < recipe.input2.fill) return false;
+		//}
 		
 		/* simplest check would usually go first, but fluid checks also do the setup and we want that to happen even without power */
 		if(this.power < getConsumption()) return false;
