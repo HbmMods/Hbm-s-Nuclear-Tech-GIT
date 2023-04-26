f91860d0603b3607ef7515e5253fb3ef0ffd59ce
JameH2 <egekarabilecen@gmail.com>
2023-04-25 21:09:11 -0600

    handle deez nuts

--- src/main/java/com/hbm/main/NEIConfig.java
+++ src/main/java/com/hbm/main/NEIConfig.java
@@ -55,6 +55,8 @@ public class NEIConfig implements IConfigureNEI {
 		registerHandler(new CrucibleCastingHandler());
 		registerHandler(new ToolingHandler());
 		registerHandler(new ConstructionHandler());
+		registerHandler(new CryoHandler());
+
 		
 		//universal boyes
 		registerHandler(new ZirnoxRecipeHandler());
