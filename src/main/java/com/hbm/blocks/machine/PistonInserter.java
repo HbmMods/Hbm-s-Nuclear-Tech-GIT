b68b83e2e28a21ed0c99a97e621e97030da806e9
Vaern <masondrown@gmail.com>
2023-05-17 07:47:32 -0700

    Finishing touches
    i mean i think that's everything?

--- src/main/java/com/hbm/blocks/machine/PistonInserter.java
+++ src/main/java/com/hbm/blocks/machine/PistonInserter.java
@@ -114,7 +114,46 @@ public class PistonInserter extends BlockContainerBase {
 		return meta != side.ordinal() && meta != side.getOpposite().ordinal();
 	}
 	
-	//TODO make item drop when block is broken
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
+		IInventory tileentityfurnace = (IInventory) world.getTileEntity(x, y, z);
+
+		if(tileentityfurnace != null) {
+			
+			ItemStack itemstack = tileentityfurnace.getStackInSlot(0);
+
+			if(itemstack != null) {
+				float f = world.rand.nextFloat() * 0.8F + 0.1F;
+				float f1 = world.rand.nextFloat() * 0.8F + 0.1F;
+				float f2 = world.rand.nextFloat() * 0.8F + 0.1F;
+				
+				while(itemstack.stackSize > 0) {
+					int j1 = world.rand.nextInt(21) + 10;
+					
+					if(j1 > itemstack.stackSize) {
+						j1 = itemstack.stackSize;
+					}
+					
+					itemstack.stackSize -= j1;
+					EntityItem entityitem = new EntityItem(world, x + f, y + f1, z + f2, new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
+					
+					if(itemstack.hasTagCompound()) {
+						entityitem.getEntityItem().setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
+					}
+
+					float f3 = 0.05F;
+					entityitem.motionX = (float) world.rand.nextGaussian() * f3;
+					entityitem.motionY = (float) world.rand.nextGaussian() * f3 + 0.2F;
+					entityitem.motionZ = (float) world.rand.nextGaussian() * f3;
+					world.spawnEntityInWorld(entityitem);
+				}
+			}
+
+			world.func_147453_f(x, y, z, block);
+		}
+
+		super.breakBlock(world, x, y, z, block, meta);
+	}
 	
 	@Override
 	public int getRenderType(){
