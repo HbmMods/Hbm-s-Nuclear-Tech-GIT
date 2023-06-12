58b08ac039be659e09fc723859fbaa06a5c9599b
Boblet <hbmmods@gmail.com>
2023-06-12 14:32:23 +0200

    explosion chunk loader config

--- src/main/java/com/hbm/entity/logic/EntityBalefire.java
+++ src/main/java/com/hbm/entity/logic/EntityBalefire.java
@@ -7,11 +7,10 @@ import com.hbm.explosion.ExplosionBalefire;
 import com.hbm.explosion.ExplosionNukeGeneric;
 import com.hbm.main.MainRegistry;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
-public class EntityBalefire extends Entity {
+public class EntityBalefire extends EntityExplosionChunkloading  {
 	
 	public int age = 0;
 	public int destructionRange = 0;
@@ -53,48 +52,46 @@ public class EntityBalefire extends Entity {
 		super(p_i1582_1_);
 	}
 
-    @Override
-	public void onUpdate() {
-        super.onUpdate();
-        	
-        if(!this.did)
-        {
-    		if(GeneralConfig.enableExtendedLogging && !worldObj.isRemote)
-    			MainRegistry.logger.log(Level.INFO, "[NUKE] Initialized BF explosion at " + posX + " / " + posY + " / " + posZ + " with strength " + destructionRange + "!");
-    		
-        	exp = new ExplosionBalefire((int)this.posX, (int)this.posY, (int)this.posZ, this.worldObj, this.destructionRange);
-        	
-        	this.did = true;
-        }
-        
-        speed += 1;	//increase speed to keep up with expansion
-        
-        boolean flag = false;
-        for(int i = 0; i < this.speed; i++)
-        {
-        	flag = exp.update();
-        	
-        	if(flag) {
-        		this.setDead();
-        	}
-        }
-        
-    	if(!mute && rand.nextInt(5) == 0)
-        	this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "random.explode", 10000.0F, 0.8F + this.rand.nextFloat() * 0.2F);
-        	
-        if(!flag) {
-        	
-        	if(!mute)
-        		this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "ambient.weather.thunder", 10000.0F, 0.8F + this.rand.nextFloat() * 0.2F);
-        	
-        	ExplosionNukeGeneric.dealDamage(this.worldObj, this.posX, this.posY, this.posZ, this.destructionRange * 2);
-        }
-        
-        age++;
-    }
-
 	@Override
-	protected void entityInit() { }
+	public void onUpdate() {
+		super.onUpdate();
+
+		if(!worldObj.isRemote) loadChunk((int) Math.floor(posX / 16D), (int) Math.floor(posZ / 16D));
+
+		if(!this.did) {
+			if(GeneralConfig.enableExtendedLogging && !worldObj.isRemote)
+				MainRegistry.logger.log(Level.INFO, "[NUKE] Initialized BF explosion at " + posX + " / " + posY + " / " + posZ + " with strength " + destructionRange + "!");
+
+			exp = new ExplosionBalefire((int) this.posX, (int) this.posY, (int) this.posZ, this.worldObj, this.destructionRange);
+
+			this.did = true;
+		}
+
+		speed += 1; // increase speed to keep up with expansion
+
+		boolean flag = false;
+		for(int i = 0; i < this.speed; i++) {
+			flag = exp.update();
+
+			if(flag) {
+				clearChunkLoader();
+				this.setDead();
+			}
+		}
+
+		if(!mute && rand.nextInt(5) == 0)
+			this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "random.explode", 10000.0F, 0.8F + this.rand.nextFloat() * 0.2F);
+
+		if(!flag) {
+
+			if(!mute)
+				this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "ambient.weather.thunder", 10000.0F, 0.8F + this.rand.nextFloat() * 0.2F);
+
+			ExplosionNukeGeneric.dealDamage(this.worldObj, this.posX, this.posY, this.posZ, this.destructionRange * 2);
+		}
+
+		age++;
+	}
 	
 	public EntityBalefire mute() {
 		this.mute = true;
