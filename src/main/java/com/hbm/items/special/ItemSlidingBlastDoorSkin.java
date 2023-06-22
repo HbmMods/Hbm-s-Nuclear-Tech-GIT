9f873e6b7611bf8e1bf78bb4a34ac34fef70030e
Toshayo <abc126cba.abc126cba@yandex.ru>
2023-06-22 08:31:45 +0200

    Skin name in door skin item

--- src/main/java/com/hbm/items/special/ItemSlidingBlastDoorSkin.java
+++ src/main/java/com/hbm/items/special/ItemSlidingBlastDoorSkin.java
@@ -4,6 +4,7 @@ import com.hbm.lib.RefStrings;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
 
 public class ItemSlidingBlastDoorSkin extends ItemDoorSkin {
     public ItemSlidingBlastDoorSkin() {
@@ -18,5 +19,10 @@ public class ItemSlidingBlastDoorSkin extends ItemDoorSkin {
         this.icons[0] = reg.registerIcon(RefStrings.MODID + ":sliding_blast_door_default");
         this.icons[1] = reg.registerIcon(RefStrings.MODID + ":sliding_blast_door_variant1");
         this.icons[2] = reg.registerIcon(RefStrings.MODID + ":sliding_blast_door_variant2");
+    }
+
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return super.getUnlocalizedName(stack) + "." + stack.getItemDamage();
     }
 }
