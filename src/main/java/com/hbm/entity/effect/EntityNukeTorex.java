35b18dda348dd17ac7c65c409c8ddd44d72b911f
Bob <hbmmods@gmail.com>
2023-08-21 23:07:47 +0200

    do the bugshaker

--- src/main/java/com/hbm/entity/effect/EntityNukeTorex.java
+++ src/main/java/com/hbm/entity/effect/EntityNukeTorex.java
@@ -37,6 +37,17 @@ public class EntityNukeTorex extends Entity {
 	}
 
 	@Override
+	@SideOnly(Side.CLIENT)
+	public int getBrightnessForRender(float p_70070_1_) {
+		return 15728880;
+	}
+
+	@Override
+	public float getBrightness(float p_70013_1_) {
+		return 1.0F;
+	}
+
+	@Override
 	public void onUpdate() {
 		
 		double s = this.getScale();
