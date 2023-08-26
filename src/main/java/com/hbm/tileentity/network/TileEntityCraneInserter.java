a050ce51da6c34798f8a535b1b07eed8ffe326d5
MartinTheDragon <martinthedragon2006@gmail.com>
2023-08-26 15:31:02 +0200

    Make conveyor crane I/O sides configurable
    Right click with a screwdriver to change output direction,
    shift-right-click to change input direction.
    
    The ton of textures were made by 70k
    
    Co-authored-by: 70000hp <105080577+70000hp@users.noreply.github.com>

--- src/main/java/com/hbm/tileentity/network/TileEntityCraneInserter.java
+++ src/main/java/com/hbm/tileentity/network/TileEntityCraneInserter.java
@@ -4,8 +4,6 @@ import com.hbm.blocks.network.CraneInserter;
 import com.hbm.inventory.container.ContainerCraneInserter;
 import com.hbm.inventory.gui.GUICraneInserter;
 import com.hbm.tileentity.IGUIProvider;
-import com.hbm.tileentity.TileEntityMachineBase;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.gui.GuiScreen;
@@ -18,7 +16,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class TileEntityCraneInserter extends TileEntityMachineBase implements IGUIProvider {
+public class TileEntityCraneInserter extends TileEntityCraneBase implements IGUIProvider {
 	
 	public static final int[] access = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 
@@ -33,18 +31,18 @@ public class TileEntityCraneInserter extends TileEntityMachineBase implements IG
 
 	@Override
 	public void updateEntity() {
-		
+		super.updateEntity();
 		if(!worldObj.isRemote) {
 
-			ForgeDirection dir = ForgeDirection.getOrientation(this.getBlockMetadata());
-			TileEntity te = worldObj.getTileEntity(xCoord - dir.offsetX, yCoord - dir.offsetY, zCoord - dir.offsetZ);
+			ForgeDirection outputSide = getOutputSide();
+			TileEntity te = worldObj.getTileEntity(xCoord + outputSide.offsetX, yCoord + outputSide.offsetY, zCoord + outputSide.offsetZ);
 			
 			int[] access = null;
 			
 			if(te instanceof ISidedInventory) {
 				ISidedInventory sided = (ISidedInventory) te;
 				//access = sided.getAccessibleSlotsFromSide(dir.ordinal());
-				access = CraneInserter.masquerade(sided, dir.ordinal());
+				access = CraneInserter.masquerade(sided, outputSide.getOpposite().ordinal());
 			}
 			
 			if(te instanceof IInventory) {
@@ -53,7 +51,7 @@ public class TileEntityCraneInserter extends TileEntityMachineBase implements IG
 					ItemStack stack = slots[i];
 					
 					if(stack != null) {
-						ItemStack ret = CraneInserter.addToInventory((IInventory) te, access, stack.copy(), dir.ordinal());
+						ItemStack ret = CraneInserter.addToInventory((IInventory) te, access, stack.copy(), outputSide.getOpposite().ordinal());
 						
 						if(ret == null || ret.stackSize != stack.stackSize) {
 							slots[i] = ret;
@@ -72,7 +70,7 @@ public class TileEntityCraneInserter extends TileEntityMachineBase implements IG
 					if(stack != null) {
 						stack = stack.copy();
 						stack.stackSize = 1;
-						ItemStack ret = CraneInserter.addToInventory((IInventory) te, access, stack.copy(), dir.ordinal());
+						ItemStack ret = CraneInserter.addToInventory((IInventory) te, access, stack.copy(), outputSide.getOpposite().ordinal());
 						
 						if(ret == null || ret.stackSize != stack.stackSize) {
 							this.decrStackSize(i, 1);
