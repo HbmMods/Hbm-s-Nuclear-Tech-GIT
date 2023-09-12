7ed53429212c118d84c6c01911773b50ff9ae31f
MartinTheDragon <martin@ntmr.dev>
2023-09-12 15:00:33 +0200

    Make ModSoundType more versatile
    - Fine-grained control over block sound subtypes
    - Allows copying from existing sounds
    - Volume and pitch envelopes
    
    Also moved custom sound types into their own class.

---
+++ src/main/java/com/hbm/blocks/ModSoundTypes.java
@@ -0,0 +1,7 @@
+package com.hbm.blocks;
+
+import net.minecraft.block.Block;
+
+class ModSoundTypes {
+    static final ModSoundType grate = ModSoundType.customStep(Block.soundTypeStone, "hbm:step.metalBlock", 0.5F, 1.0F);
+}
