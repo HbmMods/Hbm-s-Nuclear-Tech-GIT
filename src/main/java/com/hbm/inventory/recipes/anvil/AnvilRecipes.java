3d8fecd1a499ab769122073d28456702a09f32cf
TheChosenIndividual <120053489+TheChosenIndividual@users.noreply.github.com>
2023-10-31 09:57:37 -0500

    recipe for plate fuel
    self explanatory

--- src/main/java/com/hbm/inventory/recipes/anvil/AnvilRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/anvil/AnvilRecipes.java
@@ -548,6 +548,7 @@ public class AnvilRecipes {
 		constructionRecipes.add(new AnvilConstructionRecipe(new ComparableStack(ModItems.ingot_schrabidium, 1), new AnvilOutput(new ItemStack(ModItems.plate_fuel_sa326))).setTier(4));
 		constructionRecipes.add(new AnvilConstructionRecipe(new ComparableStack(ModItems.billet_ra226be, 1), new AnvilOutput(new ItemStack(ModItems.plate_fuel_ra226be))).setTier(4));
 		constructionRecipes.add(new AnvilConstructionRecipe(new ComparableStack(ModItems.billet_pu238be, 1), new AnvilOutput(new ItemStack(ModItems.plate_fuel_pu238be))).setTier(4));
+		constructionRecipes.add(new AnvilConstructionRecipe(new ComparableStack(ModItems.ingot_HPA, 1), new AnvilOutput(new ItemStack(ModItems.plate_fuel_HPA))).setTier(4));
 
 		for(int i = 0; i < 15; i += 3) {
 			constructionRecipes.add(new AnvilConstructionRecipe(new OreDictStack(IRON.plate(), 1), new AnvilOutput(new ItemStack(ModBlocks.fluid_duct_box, 1, i))).setTier(2).setOverlay(OverlayType.CONSTRUCTION));
