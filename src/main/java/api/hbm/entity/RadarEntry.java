e977d1186521186e7b97ed743c50c48c142388a7
Boblet <hbmmods@gmail.com>
2023-11-13 16:08:36 +0100

    more radar crap

--- src/main/java/api/hbm/entity/RadarEntry.java
+++ src/main/java/api/hbm/entity/RadarEntry.java
@@ -1,5 +1,7 @@
 package api.hbm.entity;
 
+import cpw.mods.fml.common.network.ByteBufUtils;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 
@@ -11,6 +13,8 @@ public class RadarEntry {
 	public int posY;
 	public int posZ;
 	public int dim;
+	
+	public RadarEntry() { } //blank ctor for packets
 	
 	public RadarEntry(String name, int level, int x, int y, int z, int dim) {
 		this.unlocalizedName = name;
@@ -31,5 +35,23 @@ public class RadarEntry {
 	
 	public RadarEntry(EntityPlayer player) {
 		this(player.getDisplayName(), IRadarDetectableNT.PLAYER, (int) Math.floor(player.posX), (int) Math.floor(player.posY), (int) Math.floor(player.posZ), player.dimension);
+	}
+	
+	public void fromBytes(ByteBuf buf) {
+		this.unlocalizedName = ByteBufUtils.readUTF8String(buf);
+		this.blipLevel = buf.readShort();
+		this.posX = buf.readInt();
+		this.posY = buf.readInt();
+		this.posZ = buf.readInt();
+		this.dim = buf.readShort();
+	}
+	
+	public void toBytes(ByteBuf buf) {
+		ByteBufUtils.writeUTF8String(buf, this.unlocalizedName);
+		buf.writeShort(this.blipLevel);
+		buf.writeInt(this.posX);
+		buf.writeInt(this.posY);
+		buf.writeInt(this.posZ);
+		buf.writeShort(this.dim);
 	}
 }
