7635dbe980195f806649249e488a16e4d85a4388
JameH2 <75378928+JameH2@users.noreply.github.com>
2023-11-22 14:38:27 -0700

    this shit sucks, dont even bother
    someone help me please god

--- src/main/java/com/hbm/entity/mob/EntityScutterfish.java
+++ src/main/java/com/hbm/entity/mob/EntityScutterfish.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.function.Predicate;
 
 import com.hbm.entity.mob.ai.EntityAIEatBread;
+import com.hbm.entity.mob.ai.EntityAISwimAway;
 import com.hbm.entity.mob.ai.EntityAISwimmingConditional;
 import com.hbm.entity.mob.ai.EntityAISwimmingVTwo;
 import com.hbm.entity.mob.ai.EntityAIWanderConditional;
@@ -40,17 +41,18 @@ public class EntityScutterfish extends EntityWaterMob implements IAnimals {
         Predicate<EntityPlayer> tooCloseCondition = player -> this.getDistanceToEntity(player) < 5.0D;
         this.getNavigator().setCanSwim(true);
 
-		//this.tasks.addTask(0, new EntityAISwimmingVTwo(this, (float) 5.0));
 		this.tasks.addTask(0, new EntityAISwimmingConditional(this, tooCloseCondition));
 		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
 		this.tasks.addTask(2, new EntityAILookIdle(this));
-		//this.tasks.addTask(4, new EntityAISwimming(this));
         this.tasks.addTask(3, new EntityAIWander(this, 1.5));
+        this.tasks.addTask(4, new EntityAISwimmingVTwo(this, 1.0));
+
         this.currentSwimTarget = Vec3.createVectorHelper(posX, posY, posZ);
 
 		this.setSize(2.0F, 2.0F);
 
 	}
+	
     protected void applyEntityAttributes() {
         super.applyEntityAttributes();
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(15.0);
@@ -72,39 +74,42 @@ public class EntityScutterfish extends EntityWaterMob implements IAnimals {
 	@Override
 	protected void updateAITasks() {
 		super.updateAITasks();
+	    this.updateSwimTarget();
+	    this.moveTowardsTarget();
 		if(!this.isInWater()) {
 		       this.rotationPitch += (float)0.2;
 		        this.rotationYaw = 0.0f;
 		        this.setJumping(true);
 		        if (this.onGround) {
-		            this.addVelocity(0.1 * EntityScutterfish.rand.nextDouble() - 0.1 * EntityScutterfish.rand.nextDouble(), 0.0, 0.1 * EntityScutterfish.rand.nextDouble() - 0.1 * EntityScutterfish.rand.nextDouble());
+		            this.addVelocity(0.4 * EntityScutterfish.rand.nextDouble() - 0.4 * EntityScutterfish.rand.nextDouble(), 0.0, 0.4 * EntityScutterfish.rand.nextDouble() - 0.4 * EntityScutterfish.rand.nextDouble());
 		        }
 		}
 	}
 
-    private void updateSwimTarget() {
-        if (this.rand.nextInt(100) == 0) {
-            double targetX = this.posX + (this.rand.nextDouble() - 0.5) * 10.0;
-            double targetY = MathHelper.clamp_double(this.posY + (this.rand.nextDouble() - 0.5) * 4.0, 0, this.worldObj.getHeight() - 1);
-            double targetZ = this.posZ + (this.rand.nextDouble() - 0.5) * 10.0;
-            this.currentSwimTarget = Vec3.createVectorHelper(targetX, targetY, targetZ);
-        }
-    }
+	private void updateSwimTarget() {
+	    if (this.rand.nextInt(50) == 0 || !this.isInWater()) { // More frequent target updates
+	        double targetX = this.posX + (this.rand.nextDouble() - 0.5) * 20.0;
+	        double targetY = MathHelper.clamp_double(this.posY + (this.rand.nextDouble() - 0.5) * 8.0, 0, this.worldObj.getHeight() - 1);
+	        double targetZ = this.posZ + (this.rand.nextDouble() - 0.5) * 20.0;
+	        this.currentSwimTarget = Vec3.createVectorHelper(targetX, targetY, targetZ);
+	    }
+	}
 
-    private void moveTowardsTarget() {
-        double deltaX = this.currentSwimTarget.xCoord - this.posX;
-        double deltaY = this.currentSwimTarget.yCoord - this.posY;
-        double deltaZ = this.currentSwimTarget.zCoord - this.posZ;
-        double distance = MathHelper.sqrt_double(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+	private void moveTowardsTarget() {
+	    double deltaX = this.currentSwimTarget.xCoord - this.posX;
+	    double deltaY = this.currentSwimTarget.yCoord - this.posY;
+	    double deltaZ = this.currentSwimTarget.zCoord - this.posZ;
+	    double distance = MathHelper.sqrt_double(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
 
-        double dirX = deltaX / distance;
-        double dirY = deltaY / distance;
-        double dirZ = deltaZ / distance;
+	    double dirX = deltaX / distance;
+	    double dirY = deltaY / distance;
+	    double dirZ = deltaZ / distance;
 
-        this.motionX += dirX * 0.1;
-        this.motionY += dirY * 0.1;
-        this.motionZ += dirZ * 0.1;
-    }
+	    double speed = 0.1; // You can adjust this value for different speeds
+	    this.motionX += dirX * speed + (rand.nextDouble() - 0.5) * 0.02; // Added randomness
+	    this.motionY += dirY * speed + (rand.nextDouble() - 0.5) * 0.02;
+	    this.motionZ += dirZ * speed + (rand.nextDouble() - 0.5) * 0.02;
+	}
 
     @Override
     public void moveEntityWithHeading(float strafe, float forward) {
--- src/main/java/com/hbm/entity/mob/ai/EntityAISwimmingVTwo.java
+++ src/main/java/com/hbm/entity/mob/ai/EntityAISwimmingVTwo.java
@@ -4,54 +4,46 @@ import com.hbm.entity.mob.EntityScutterfish;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.RandomPositionGenerator;
 import net.minecraft.util.Vec3;
 
 public class EntityAISwimmingVTwo extends EntityAIBase {
     private EntityScutterfish fish;
-    private float speed;
-    double xPosition;
-    double yPosition;
-    double zPosition;
+    private double xPosition;
+    private double yPosition;
+    private double zPosition;
+    private double speed;
 
-    public EntityAISwimmingVTwo(EntityScutterfish fish, float speed) {
+    public EntityAISwimmingVTwo(EntityScutterfish fish, double speed) {
         this.fish = fish;
         this.speed = speed;
-        this.setMutexBits(4);
-        fish.getNavigator().setCanSwim(true);
+        this.setMutexBits(1); // Set the mutex bits as needed
     }
 
     @Override
     public boolean shouldExecute() {
-        // This AI should only execute if the fish is in water
-        if (fish.isInWater() && fish.getRNG().nextDouble() < 0.5) {
-            // Randomly choose a destination within the water
-            xPosition = fish.posX + (fish.getRNG().nextDouble() - 0.5) * 8.0;
-            yPosition = fish.posY + (fish.getRNG().nextDouble() - 0.5) * 4.0;
-            zPosition = fish.posZ + (fish.getRNG().nextDouble() - 0.5) * 8.0;
+        if (this.fish.getRNG().nextInt(120) != 0) {
+            return false;
+        }
+        Vec3 randomTarget = RandomPositionGenerator.findRandomTarget(this.fish, 10, 7);
+        if (randomTarget == null) {
+            return false;
+        } else {
+            this.xPosition = randomTarget.xCoord;
+            this.yPosition = randomTarget.yCoord;
+            this.zPosition = randomTarget.zCoord;
             return true;
         }
-        return false;
     }
 
     @Override
     public boolean continueExecuting() {
-        // Continue executing as long as the target position is in water and not reached
-        return !fish.getNavigator().noPath() && fish.isInWater();
+        return !this.fish.getNavigator().noPath();
     }
 
     @Override
     public void startExecuting() {
-        // Start moving towards the target position
-        fish.getNavigator().tryMoveToXYZ(xPosition, yPosition, zPosition, speed);
-    }
-
-    @Override
-    public void resetTask() {
-        // Reset the pathfinder when the task is interrupted or completed
-        xPosition = 0.0;
-        yPosition = 0.0;
-        zPosition = 0.0;
-        fish.getNavigator().clearPathEntity();
+        this.fish.getNavigator().tryMoveToXYZ(this.xPosition, this.yPosition, this.zPosition, this.speed);
     }
 }
 
