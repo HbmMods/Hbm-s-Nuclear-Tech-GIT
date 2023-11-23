e40343b6dd6fed2d479d72b51b0b227b91bebb33
Bob <hbmmods@gmail.com>
2023-11-23 20:39:54 +0100

    crunchier tier 1 missiles

--- src/main/java/com/hbm/render/entity/rocket/RenderMissileGeneric.java
+++ src/main/java/com/hbm/render/entity/rocket/RenderMissileGeneric.java
@@ -16,29 +16,31 @@ public class RenderMissileGeneric extends Render {
 	}
 
 	@Override
-	public void doRender(Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_) {
+	public void doRender(Entity entity, double x, double y, double z, float p_76986_8_, float interp) {
 
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float) p_76986_2_, (float) p_76986_4_, (float) p_76986_6_);
-		GL11.glRotatef(p_76986_1_.prevRotationYaw + (p_76986_1_.rotationYaw - p_76986_1_.prevRotationYaw) * p_76986_9_ - 90.0F, 0.0F, 1.0F, 0.0F);
-		GL11.glRotatef(p_76986_1_.prevRotationPitch + (p_76986_1_.rotationPitch - p_76986_1_.prevRotationPitch) * p_76986_9_, 0.0F, 0.0F, 1.0F);
+		GL11.glTranslatef((float) x, (float) y, (float) z);
+		GL11.glRotatef(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * interp - 90.0F, 0.0F, 1.0F, 0.0F);
+		GL11.glRotatef(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * interp, 0.0F, 0.0F, 1.0F);
 
-		if(p_76986_1_ instanceof EntityMissileGeneric)
+		if(entity instanceof EntityMissileGeneric)
 			bindTexture(ResourceManager.missileV2_HE_tex);
-		if(p_76986_1_ instanceof EntityMissileIncendiary)
+		if(entity instanceof EntityMissileIncendiary)
 			bindTexture(ResourceManager.missileV2_IN_tex);
-		if(p_76986_1_ instanceof EntityMissileCluster)
+		if(entity instanceof EntityMissileCluster)
 			bindTexture(ResourceManager.missileV2_CL_tex);
-		if(p_76986_1_ instanceof EntityMissileBunkerBuster)
+		if(entity instanceof EntityMissileBunkerBuster)
 			bindTexture(ResourceManager.missileV2_BU_tex);
 
-		if(p_76986_1_ instanceof EntityMissileAntiBallistic) {
+		if(entity instanceof EntityMissileAntiBallistic) {
 			bindTexture(ResourceManager.missileAA_tex);
 			GL11.glShadeModel(GL11.GL_SMOOTH);
 			ResourceManager.missileABM.renderAll();
 			GL11.glShadeModel(GL11.GL_FLAT);
 		} else {
+			GL11.glShadeModel(GL11.GL_SMOOTH);
 			ResourceManager.missileV2.renderAll();
+			GL11.glShadeModel(GL11.GL_FLAT);
 		}
 		GL11.glPopMatrix();
 	}
