05ddda7cf7f83cffb5b5ed786653710558942283
JameH2 <75378928+JameH2@users.noreply.github.com>
2023-12-30 15:58:16 -0700

    forgot

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -3,7 +3,7 @@ package com.hbm.lib;
 public class RefStrings {
 	public static final String MODID = "hbm";
 	public static final String NAME = "Hbm's Nuclear Tech Mod";
-	public static final String VERSION = "1.0.27 BETA (JH750)"; // i wonder how the fuck i didnt get mad at the fact that it kept ending with "9"
+	public static final String VERSION = "1.0.27 BETA (JH780)"; // i wonder how the fuck i didnt get mad at the fact that it kept ending with "9"
 	
 	//James Versioning
 	// (HBM VER NUMBER) + (SIGNATURE) + (VERSION)
