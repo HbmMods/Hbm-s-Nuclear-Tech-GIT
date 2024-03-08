8323be427a08963d54a2abb405687f3a86f70872
poloniun <113172472+poloniun@users.noreply.github.com>
2024-03-08 14:42:59 +0800

    Add files via upload

--- src/main/java/com/hbm/saveddata/satellites/SatelliteLunarMiner.java
+++ src/main/java/com/hbm/saveddata/satellites/SatelliteLunarMiner.java
@@ -8,13 +8,18 @@ import net.minecraft.item.ItemStack;
 public class SatelliteLunarMiner extends SatelliteMiner {
     static {
         registerCargo(SatelliteLunarMiner.class, new WeightedRandomObject[] {
-                new WeightedRandomObject(new ItemStack(ModBlocks.moon_turf, 48), 5),
-                new WeightedRandomObject(new ItemStack(ModBlocks.moon_turf, 32), 7),
-                new WeightedRandomObject(new ItemStack(ModBlocks.moon_turf, 16), 5),
-                new WeightedRandomObject(new ItemStack(ModItems.powder_lithium, 3), 5),
-                new WeightedRandomObject(new ItemStack(ModItems.powder_iron, 3), 5),
-                new WeightedRandomObject(new ItemStack(ModItems.crystal_iron, 1), 1),
-                new WeightedRandomObject(new ItemStack(ModItems.crystal_lithium, 1), 1)
+                new WeightedRandomObject(new ItemStack(ModBlocks.moon_turf, 8), 15),
+                new WeightedRandomObject(new ItemStack(ModBlocks.moon_turf, 16), 15),
+                new WeightedRandomObject(new ItemStack(ModItems.powder_lithium, 3), 15),
+                new WeightedRandomObject(new ItemStack(ModItems.powder_iron, 3), 15),
+	new WeightedRandomObject(new ItemStack(ModItems.powder_nitan_mix, 2), 10),
+	new WeightedRandomObject(new ItemStack(ModItems.powder_combine_steel, 1), 8),
+	new WeightedRandomObject(new ItemStack(ModItems.powder_magic, 1), 7),
+	new WeightedRandomObject(new ItemStack(ModItems.powder_dineutronium, 1), 7),
+	new WeightedRandomObject(new ItemStack(ModItems.ingot_technetium, 1), 10),
+
+	new WeightedRandomObject(new ItemStack(ModItems.powder_meteorite, 1), 8)
+
         });
     }
 }
