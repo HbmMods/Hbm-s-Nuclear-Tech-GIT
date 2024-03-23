d71fa93ab74347a95ac993e7bcf914811bc1998d
Vaern <masondrown@gmail.com>
2024-03-23 16:04:56 -0700

    argh

--- src/main/java/com/hbm/inventory/container/ContainerFEL.java
+++ src/main/java/com/hbm/inventory/container/ContainerFEL.java
@@ -1,7 +1,9 @@
 package com.hbm.inventory.container;
 
+import com.hbm.items.ModItems;
 import com.hbm.tileentity.machine.TileEntityFEL;
 
+import api.hbm.energy.IBatteryItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
@@ -33,31 +35,35 @@ public class ContainerFEL extends Container {
 	}
 
 	@Override
-	public ItemStack transferStackInSlot(EntityPlayer p_82846_1_, int par2) {
-		ItemStack var3 = null;
-		Slot var4 = (Slot) this.inventorySlots.get(par2);
+	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+		ItemStack rStack = null;
+		Slot slot = (Slot) this.inventorySlots.get(index);
 
-		if(var4 != null && var4.getHasStack()) {
-			ItemStack var5 = var4.getStack();
-			var3 = var5.copy();
+		if(slot != null && slot.getHasStack()) {
+			ItemStack stack = slot.getStack();
+			rStack = stack.copy();
 
-			if(par2 == 0) {
-				if(!this.mergeItemStack(var5, 1, this.inventorySlots.size(), false)) {
+			if(index == 0) {
+				if(!this.mergeItemStack(stack, 1, this.inventorySlots.size(), false)) {
 					return null;
 				}
 			} else {
-				if(!this.mergeItemStack(var5, 0, 1, false))
-					return null;
+				
+				if(rStack.getItem() instanceof IBatteryItem || rStack.getItem() == ModItems.battery_creative) {
+					if(!this.mergeItemStack(stack, 0, 1, false)) return null;
+				} else {
+					if(!this.mergeItemStack(stack, 1, 2, false)) return null;
+				}
 			}
 
-			if(var5.stackSize == 0) {
-				var4.putStack((ItemStack) null);
+			if(stack.stackSize == 0) {
+				slot.putStack((ItemStack) null);
 			} else {
-				var4.onSlotChanged();
+				slot.onSlotChanged();
 			}
 		}
 
-		return var3;
+		return rStack;
 	}
 
 	@Override
