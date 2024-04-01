84c24a619e706cbe123ff40b64e3d9842bd82437
LegendaryDoge30 <legendarydoge9@gmail.com>
2024-04-01 17:40:50 +0200

    blorbcks

--- src/main/java/com/hbm/handler/FuelHandler.java
+++ src/main/java/com/hbm/handler/FuelHandler.java
@@ -18,29 +18,32 @@ public class FuelHandler implements IFuelHandler {
 		
 		int single = 200;
 
-		if(fuel.getItem().equals(ModItems.solid_fuel))						return single * 16;
-		if(fuel.getItem().equals(ModItems.solid_fuel_presto))				return single * 40;
-		if(fuel.getItem().equals(ModItems.solid_fuel_presto_triplet))		return single * 200;
-		if(fuel.getItem().equals(ModItems.solid_fuel_bf))					return single * 160;
-		if(fuel.getItem().equals(ModItems.solid_fuel_presto_bf))			return single * 400;
-		if(fuel.getItem().equals(ModItems.solid_fuel_presto_triplet_bf))	return single * 2000;
-		if(fuel.getItem().equals(ModItems.rocket_fuel))						return single * 32;
-		
-		if(fuel.getItem() == ModItems.biomass)								return single * 2;
-		if(fuel.getItem() == ModItems.biomass_compressed)					return single * 4;
-		if(fuel.getItem() == ModItems.powder_coal)							return single * 8;
-		if(fuel.getItem() == ModItems.scrap)								return single / 4;
-		if(fuel.getItem() == ModItems.dust)									return single / 8;
-		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_scrap))	return single * 2;
-		if(fuel.getItem() == ModItems.powder_fire)							return 6400;
-		if(fuel.getItem() == ModItems.lignite)								return 1200;
-		if(fuel.getItem() == ModItems.powder_lignite)						return 1200;
-		if(fuel.getItem() == ModItems.coke)									return single * 16;
-		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_coke))	return single * 160;
-		if(fuel.getItem() == ModItems.book_guide)							return single;
-		if(fuel.getItem() == ModItems.coal_infernal)						return 4800;
-		if(fuel.getItem() == ModItems.crystal_coal)							return 6400;
-		if(fuel.getItem() == ModItems.powder_sawdust)						return single / 2;
+		if(fuel.getItem().equals(ModItems.solid_fuel))								return single * 16;
+		if(fuel.getItem().equals(ModItems.solid_fuel_presto))						return single * 40;
+		if(fuel.getItem().equals(ModItems.solid_fuel_presto_triplet))				return single * 200;
+		if(fuel.getItem().equals(ModItems.solid_fuel_bf))							return single * 160;
+		if(fuel.getItem().equals(ModItems.solid_fuel_presto_bf))					return single * 400;
+		if(fuel.getItem().equals(ModItems.solid_fuel_presto_triplet_bf))			return single * 2000;
+		if(fuel.getItem().equals(ModItems.rocket_fuel))								return single * 32;
+
+		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_solid_fuel))		return single * 144;
+		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_rocket_fuel))	return single * 288;
+		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_solid_fuel_bf))	return single * 1440;
+		if(fuel.getItem() == ModItems.biomass)										return single * 2;
+		if(fuel.getItem() == ModItems.biomass_compressed)							return single * 4;
+		if(fuel.getItem() == ModItems.powder_coal)									return single * 8;
+		if(fuel.getItem() == ModItems.scrap)										return single / 4;
+		if(fuel.getItem() == ModItems.dust)											return single / 8;
+		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_scrap))			return single * 2;
+		if(fuel.getItem() == ModItems.powder_fire)									return 6400;
+		if(fuel.getItem() == ModItems.lignite)										return 1200;
+		if(fuel.getItem() == ModItems.powder_lignite)								return 1200;
+		if(fuel.getItem() == ModItems.coke)											return single * 16;
+		if(fuel.getItem() == Item.getItemFromBlock(ModBlocks.block_coke))			return single * 160;
+		if(fuel.getItem() == ModItems.book_guide)									return single;
+		if(fuel.getItem() == ModItems.coal_infernal)								return 4800;
+		if(fuel.getItem() == ModItems.crystal_coal)									return 6400;
+		if(fuel.getItem() == ModItems.powder_sawdust)								return single / 2;
 		
 		if(fuel.getItem() == ModItems.briquette) {
 			int meta = fuel.getItemDamage();
