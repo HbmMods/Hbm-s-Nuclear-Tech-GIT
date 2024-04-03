fb765174dd342373e0d8d80bdbe205a33206e1de
70000hp <105080577+70000hp@users.noreply.github.com>
2024-04-03 17:42:10 -0400

    moremore light logic

--- src/main/java/com/hbm/entity/mob/glyphid/EntityGlyphidScout.java
+++ src/main/java/com/hbm/entity/mob/glyphid/EntityGlyphidScout.java
@@ -336,15 +336,13 @@ public class EntityGlyphidScout extends EntityGlyphid {
 		int y = MathHelper.floor_double(this.boundingBox.minY);
 		int z = MathHelper.floor_double(this.posZ);
 
-		int light = this.worldObj.getBlockLightValue(x, y, z);
+		int skylightSubtracted = this.worldObj.skylightSubtracted;
 
-		if(this.worldObj.isThundering()) {
-			int skylightSubtracted = this.worldObj.skylightSubtracted;
-			this.worldObj.skylightSubtracted = 10;
-			light = this.worldObj.getBlockLightValue(x, y, z);
-			this.worldObj.skylightSubtracted = skylightSubtracted;
-		}
-			
+		if(this.worldObj.isThundering()) this.worldObj.skylightSubtracted = 10;
+		int light = worldObj.getChunkFromChunkCoords(x >> 4, z >> 4).getBlockLightValue(x & 15, y, z & 15, worldObj.skylightSubtracted);
+
+		this.worldObj.skylightSubtracted = skylightSubtracted;
 		return light <= 7;
+
 	}
 }
