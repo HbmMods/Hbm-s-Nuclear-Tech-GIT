e9c4c503e5e5951ea12d239c6479508687c46fe6
JameH2 <egekarabilecen@gmail.com>
2024-05-23 19:19:24 -0600

    bruh

--- src/main/java/com/hbm/dim/WorldProviderCelestial.java
+++ src/main/java/com/hbm/dim/WorldProviderCelestial.java
@@ -227,7 +227,7 @@ public abstract class WorldProviderCelestial extends WorldProvider {
 
 		if(atmosphere == null) return sunBrightness;
 
-		if(atmosphere.getPressure() < 2F) {
+		if(atmosphere.getPressure() > 2F) {
 			return sunBrightness *= 0.3F;
 		}
 
