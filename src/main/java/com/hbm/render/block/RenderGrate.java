7eb8900c13412be9850df3900a58c5c16c38ad5f
George Paton <georgep@webforcefive.com.au>
2024-06-06 21:15:43 +1000

    crouch-clicking is grate

--- src/main/java/com/hbm/render/block/RenderGrate.java
+++ src/main/java/com/hbm/render/block/RenderGrate.java
@@ -22,7 +22,9 @@ public class RenderGrate implements ISimpleBlockRenderingHandler {
 		tessellator.setBrightness(block.getMixedBrightnessForBlock(world, x, y, z));
 		tessellator.setColorOpaque_F(1, 1, 1);
 
-		renderer.setRenderBounds(0.0D, meta * 0.125D, 0D, 1D, meta * 0.125D + 0.125D, 1D);
+		float fy = ((BlockGrate)block).getY(meta);
+
+		renderer.setRenderBounds(0.0D, fy, 0D, 1D, fy + 0.125D, 1D);
 		renderer.renderStandardBlock(block, x, y, z);
 		return true;
 	}
