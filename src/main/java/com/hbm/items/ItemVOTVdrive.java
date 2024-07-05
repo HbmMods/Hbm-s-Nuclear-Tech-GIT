394a06aa57dbddc85882e7a942c4a4f5a532377f
George Paton <georgewpaton@me.com>
2024-07-05 18:25:35 +1000

    make it possible to change the drive more than once per landing

--- src/main/java/com/hbm/items/ItemVOTVdrive.java
+++ src/main/java/com/hbm/items/ItemVOTVdrive.java
@@ -114,7 +114,7 @@ public class ItemVOTVdrive extends ItemEnumMulti {
 		if(player.ridingEntity != null && player.ridingEntity instanceof EntityRideableRocket) {
 			EntityRideableRocket rocket = (EntityRideableRocket) player.ridingEntity;
 
-			if(rocket.getState() == RocketState.LANDED) {
+			if(rocket.getState() == RocketState.LANDED || rocket.getState() == RocketState.AWAITING) {
 				// Replace our held stack with the rocket drive and place our held drive into the rocket
 				if(rocket.navDrive != null) {
 					newStack = rocket.navDrive;
