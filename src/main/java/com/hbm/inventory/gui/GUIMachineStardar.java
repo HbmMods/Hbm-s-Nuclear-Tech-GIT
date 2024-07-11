68e1cd00d92b841de04d0374786134370e947d8d
JameH2 <egekarabilecen@gmail.com>
2024-07-11 03:48:22 -0600

    ᛁ ᚷᛁᚢᛖ ᚢᛈ ᚺᛟᛚᛁ ᚠᚢᚲᚲᛁᛜ ᛊᚺᛁᛏ

--- src/main/java/com/hbm/inventory/gui/GUIMachineStardar.java
+++ src/main/java/com/hbm/inventory/gui/GUIMachineStardar.java
@@ -5,6 +5,7 @@ import org.lwjgl.opengl.GL11;
 
 import com.hbm.dim.SkyProviderCelestial;
 import com.hbm.dim.SolarSystem;
+import com.hbm.interfaces.Spaghetti;
 import com.hbm.inventory.container.ContainerStardar;
 import com.hbm.lib.RefStrings;
 import com.hbm.packet.NBTControlPacket;
@@ -13,7 +14,7 @@ import com.hbm.tileentity.machine.TileEntityElectrolyser;
 import com.hbm.tileentity.machine.TileEntityMachineStardar;
 import com.hbm.util.AstronomyUtil;
 
-import net.java.games.input.Keyboard;
+import org.lwjgl.input.Keyboard;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -21,6 +22,17 @@ import net.minecraft.inventory.Container;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 
+
+
+//dear james:
+/*
+ * jesus fucking christ, this is exactly the kind of shit i was fearing when i initially thought of ways to have a planet selector screen.
+ * its not that i cant finish this, or the fact that this shit took me an ENTIRE FUCKING NIGHT. its the fact how fucking *close* i was
+ * and the fact that i cannot under my utter retardedness cannot figure it out holy shit and i might actually never will.
+ * its over for me as a programmer.
+ */
+
+@Spaghetti("dont wanna put it in the annotation")
 public class GUIMachineStardar extends GuiInfoContainer {
 	public static ResourceLocation texture = new ResourceLocation(RefStrings.MODID + ":textures/gui/machine/gui_stardar.png");
 	private static final ResourceLocation nightTexture = new ResourceLocation(RefStrings.MODID, "textures/misc/space/night.png");
@@ -49,32 +61,64 @@ public class GUIMachineStardar extends GuiInfoContainer {
 		int ymass = (mY - sY);
 		imgoingtojumpoffabuildingbutwithloveandroses = nmass;
 		nmass2 = ymass;
-        System.out.println("sxw 2;" + nmass);
-        System.out.println("mxw 2;" + ymass);
 
-		drawTexturedModalRect(guiLeft + imgoingtojumpoffabuildingbutwithloveandroses, guiTop + nmass2, 0, 0, xSize, ySize);
+		
+		drawTexturedModalRect(guiLeft , guiTop, mX, mY, xSize, ySize);
+        if (Keyboard.isKeyDown(Keyboard.KEY_W))
+        {
+        	mY--;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_S))        
+        {
+        	mY++;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_A))        
+        { 
+        	mX--;
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_D))        
+        {
+        	mX++;
+        }
 		popScissor();
 	}
+	
+	
     @Override
     public void handleMouseInput() {
         super.handleMouseInput();
+        /*
         int button = Mouse.getEventButton();
 
         if (button == 0 && !Mouse.getEventButtonState()) { 
+        	//sX = mX;
+        	//sY = mY;
+
         	imgoingtojumpoffabuildingbutwithloveandroses = (mX - sX);
         	nmass2 = (mY - sY);
-            System.out.println("sx ;" + sX);
-            System.out.println("mx ;" + mX);
+    		System.out.println("i let go of the mouse here (still the difference) x: " + imgoingtojumpoffabuildingbutwithloveandroses);
+    		System.out.println("i let go of the mouse here (still the difference) y: " + nmass2);
+        }       
+        if (button == 0 && Mouse.getEventButtonState()) {
+            sX = Mouse.getEventX() * this.width / this.mc.displayWidth;
+            sY = this.height - Mouse.getEventY() * this.height / this.mc.displayHeight - 1;
+
         }
+        */
     }
 	@Override
 	protected void mouseClickMove(int x, int y, int p_146273_3_, long p_146273_4_) {
 		
 		super.mouseClickMove(x, y, p_146273_3_, p_146273_4_);
+		
+		/*
 		mX = x;
 		mY = y;
-		System.out.println(mX);
-		System.out.println(mY);
+		*/
+
 
 	}
 	
@@ -87,14 +131,19 @@ public class GUIMachineStardar extends GuiInfoContainer {
 	@Override 
 	protected void mouseClicked(int x, int y, int i) {
 		super.mouseClicked(x, y, i);
-		System.out.println(mX);
 
+		/*
 		sX = x;
 		sY = y;
 		mX = x;
 		mY = y;
 		System.out.println("start x: " + sX);
 		System.out.println("start y: " + sY);
+		System.out.println("mouse x: " + mX);
+		System.out.println("mouse y: " + mY);
+		System.out.println("the difference in x: " + imgoingtojumpoffabuildingbutwithloveandroses);
+		System.out.println("the difference in y: " + nmass2);
 	}
-
+	*/
+	}
 }
