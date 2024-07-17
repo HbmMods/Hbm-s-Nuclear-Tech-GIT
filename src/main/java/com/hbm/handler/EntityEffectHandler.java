3db31188659c9bbc96ecf5f299d11956b1c67ee8
JameH2 <egekarabilecen@gmail.com>
2024-07-17 00:37:24 -0600

    giggles

--- src/main/java/com/hbm/handler/EntityEffectHandler.java
+++ src/main/java/com/hbm/handler/EntityEffectHandler.java
@@ -11,6 +11,7 @@ import com.hbm.config.WorldConfig;
 import com.hbm.dim.trait.CBT_Atmosphere;
 import com.hbm.dim.trait.CBT_Atmosphere.FluidEntry;
 import com.hbm.entity.missile.EntityRideableRocket;
+import com.hbm.entity.mob.EntityCyberCrab;
 import com.hbm.entity.mob.glyphid.EntityGlyphid;
 import com.hbm.explosion.ExplosionNukeSmall;
 import com.hbm.extprop.HbmLivingProps;
@@ -302,6 +303,7 @@ public class EntityEffectHandler {
 		if(entity.worldObj.isRemote) return;
 		if(entity instanceof EntityGlyphid) return; // can't suffocate the bastards
 		if(entity.ridingEntity != null && entity.ridingEntity instanceof EntityRideableRocket) return; // breathe easy in your ship
+		if(entity instanceof EntityCyberCrab) return; // machines
 
 		if (!ArmorUtil.checkForOxy(entity)) {
 			HbmLivingProps.setOxy(entity, HbmLivingProps.getOxy(entity) - 1);
