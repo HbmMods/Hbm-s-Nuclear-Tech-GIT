afd3fcb9d19d83a86ab112244a099f36b3013217
JameH2 <egekarabilecen@gmail.com>
2024-07-17 23:58:23 -0600

    added ravines...?

--- src/main/java/com/hbm/dim/moho/ChunkProviderMoho.java
+++ src/main/java/com/hbm/dim/moho/ChunkProviderMoho.java
@@ -8,15 +8,19 @@ import com.hbm.dim.ExperimentalCaveGenerator;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.MapGenRavine;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
 public class ChunkProviderMoho extends ChunkProviderCelestial {
 
 	private ExperimentalCaveGenerator caveGenV2 = new ExperimentalCaveGenerator(1, 52, 10.0F);
+	private MapGenRavine rgen = new MapGenRavine();
 
 	public ChunkProviderMoho(World world, long seed, boolean hasMapFeatures) {
 		super(world, seed, hasMapFeatures);
 		caveGenV2 = (ExperimentalCaveGenerator) TerrainGen.getModdedMapGen(caveGenV2, CUSTOM);
+		rgen = (MapGenRavine) TerrainGen.getModdedMapGen(rgen, RAVINE);
+
 		caveGenV2.stoneBlock = ModBlocks.moho_stone;
 		
 		stoneBlock = ModBlocks.moho_stone;
@@ -29,7 +33,7 @@ public class ChunkProviderMoho extends ChunkProviderCelestial {
 
 		// how many times do I gotta say BEEEEG
 		this.caveGenV2.func_151539_a(this, worldObj, x, z, buffer.blocks);
-		
+		this.rgen.func_151539_a(this, worldObj, x, z, buffer.blocks);
 		return buffer;
 	}
 
