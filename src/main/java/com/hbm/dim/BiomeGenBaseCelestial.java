7b0feace0ed2ce3f1817c58f926c579efdc1eef2
George Paton <georgewpaton@me.com>
2024-08-10 10:09:48 +1000

    Actually fix the mobs from other mods spawning and dying immediately bug

--- src/main/java/com/hbm/dim/BiomeGenBaseCelestial.java
+++ src/main/java/com/hbm/dim/BiomeGenBaseCelestial.java
@@ -8,23 +8,24 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public abstract class BiomeGenBaseCelestial extends BiomeGenBase {
 
-    protected List<SpawnListEntry> creatures = new ArrayList<SpawnListEntry>();
-    protected List<SpawnListEntry> monsters = new ArrayList<SpawnListEntry>();
-    protected List<SpawnListEntry> waterCreatures = new ArrayList<SpawnListEntry>();
-    protected List<SpawnListEntry> caveCreatures = new ArrayList<SpawnListEntry>();
+    protected ArrayList<SpawnListEntry> creatures = new ArrayList<SpawnListEntry>();
+    protected ArrayList<SpawnListEntry> monsters = new ArrayList<SpawnListEntry>();
+    protected ArrayList<SpawnListEntry> waterCreatures = new ArrayList<SpawnListEntry>();
+    protected ArrayList<SpawnListEntry> caveCreatures = new ArrayList<SpawnListEntry>();
 
     public BiomeGenBaseCelestial(int id) {
         super(id);
     }
 
+    // Returns a copy of the lists to prevent them being modified
     @SuppressWarnings("rawtypes")
     @Override
     public List getSpawnableList(EnumCreatureType type) {
         switch(type) {
-        case monster: return monsters;
-        case creature: return creatures;
-        case waterCreature: return waterCreatures;
-        case ambient: return caveCreatures;
+        case monster: return (List)monsters.clone();
+        case creature: return (List)creatures.clone();
+        case waterCreature: return (List)waterCreatures.clone();
+        case ambient: return (List)caveCreatures.clone();
         default: return new ArrayList<SpawnListEntry>();
         }
     }
