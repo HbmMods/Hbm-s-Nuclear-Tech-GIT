e52732193c716fa932f2f76f5adcab9dbbe01771
George Paton <georgewpaton@me.com>
2024-08-13 12:04:18 +1000

    make two transporter rockets at a time

--- src/main/java/com/hbm/inventory/recipes/AssemblerRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/AssemblerRecipes.java
@@ -714,12 +714,12 @@ public class AssemblerRecipes extends SerializableRecipe {
 				new ComparableStack(ModItems.motor, 1),
 			}, 300);
 		
-		makeRecipe(new ComparableStack(ModBlocks.transporter_rocket, 1), new AStack[] {
+		makeRecipe(new ComparableStack(ModBlocks.transporter_rocket, 2), new AStack[] {
 				new OreDictStack(TI.plateWelded(), 4),
-				new ComparableStack(ModBlocks.crate_iron, 2), 
+				new ComparableStack(ModBlocks.crate_iron, 2),
 				//new ComparableStack(ModItems.circuit, 1, EnumCircuitType.AREO), //gate,
 				new ComparableStack(ModItems.tank_steel, 8),
-				new ComparableStack(ModItems.thruster_small, 1), 
+				new ComparableStack(ModItems.thruster_small, 1),
 				new ComparableStack(ModItems.circuit, 1, EnumCircuitType.AVIONICS),
 
 			}, 300);
