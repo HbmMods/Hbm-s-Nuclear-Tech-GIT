dad2f22bb14867f66931af26ae8edc397805c323
George Paton <georgewpaton@me.com>
2024-09-10 16:39:26 +1000

    fix build

--- src/main/java/com/hbm/inventory/gui/GUIOrbitalStationComputer.java
+++ src/main/java/com/hbm/inventory/gui/GUIOrbitalStationComputer.java
@@ -6,7 +6,7 @@ import org.lwjgl.opengl.GL11;
 import com.hbm.dim.orbit.OrbitalStation;
 import com.hbm.inventory.container.ContainerOrbitalStationComputer;
 import com.hbm.lib.RefStrings;
-import com.hbm.packet.NBTControlPacket;
+import com.hbm.packet.toserver.NBTControlPacket;
 import com.hbm.packet.PacketDispatcher;
 import com.hbm.tileentity.machine.TileEntityOrbitalStationComputer;
 
