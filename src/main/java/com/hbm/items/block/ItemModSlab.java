97deb9930f81a31dec01bfc947c1bceeef25218b
abel1502 <terragame.abel@gmail.com>
2024-10-30 22:39:06 +0300

    Add missing config check

--- src/main/java/com/hbm/items/block/ItemModSlab.java
+++ src/main/java/com/hbm/items/block/ItemModSlab.java
@@ -2,6 +2,7 @@ package com.hbm.items.block;
 
 import com.hbm.blocks.ModBlocks;
 import com.hbm.interfaces.NotableComments;
+import com.hbm.config.GeneralConfig;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSlab;
@@ -18,14 +19,16 @@ public class ItemModSlab extends ItemSlab {
 		if(b == ModBlocks.concrete_slab || b == ModBlocks.concrete_double_slab) return (BlockSlab) ModBlocks.concrete_slab;
 		if(b == ModBlocks.concrete_brick_slab || b == ModBlocks.concrete_brick_double_slab) return (BlockSlab) ModBlocks.concrete_brick_slab;
 		if(b == ModBlocks.brick_slab || b == ModBlocks.brick_double_slab) return (BlockSlab) ModBlocks.brick_slab;
-		int indexColoredSingle = ModBlocks.concrete_colored_slabs.indexOf(b);
-		if(indexColoredSingle >= 0) return (BlockSlab)b;
-		int indexColoredDouble = ModBlocks.concrete_colored_double_slabs.indexOf(b);
-		if(indexColoredDouble >= 0) return (BlockSlab)ModBlocks.concrete_colored_slabs.get(indexColoredDouble);
-		int indexColoredExtSingle = ModBlocks.concrete_colored_ext_slabs.indexOf(b);
-		if(indexColoredExtSingle >= 0) return (BlockSlab)b;
-		int indexColoredExtDouble = ModBlocks.concrete_colored_ext_double_slabs.indexOf(b);
-		if(indexColoredExtDouble >= 0) return (BlockSlab)ModBlocks.concrete_colored_ext_slabs.get(indexColoredExtDouble);
+		if (GeneralConfig.enableColoredStairsSlabs) {
+			int indexColoredSingle = ModBlocks.concrete_colored_slabs.indexOf(b);
+			if(indexColoredSingle >= 0) return (BlockSlab)b;
+			int indexColoredDouble = ModBlocks.concrete_colored_double_slabs.indexOf(b);
+			if(indexColoredDouble >= 0) return (BlockSlab)ModBlocks.concrete_colored_slabs.get(indexColoredDouble);
+			int indexColoredExtSingle = ModBlocks.concrete_colored_ext_slabs.indexOf(b);
+			if(indexColoredExtSingle >= 0) return (BlockSlab)b;
+			int indexColoredExtDouble = ModBlocks.concrete_colored_ext_double_slabs.indexOf(b);
+			if(indexColoredExtDouble >= 0) return (BlockSlab)ModBlocks.concrete_colored_ext_slabs.get(indexColoredExtDouble);
+		}
 		return null;
 	}
 	
@@ -33,14 +36,16 @@ public class ItemModSlab extends ItemSlab {
 		if(b == ModBlocks.concrete_slab || b == ModBlocks.concrete_double_slab) return (BlockSlab) ModBlocks.concrete_double_slab;
 		if(b == ModBlocks.concrete_brick_slab || b == ModBlocks.concrete_brick_double_slab) return (BlockSlab) ModBlocks.concrete_brick_double_slab;
 		if(b == ModBlocks.brick_slab || b == ModBlocks.brick_double_slab) return (BlockSlab) ModBlocks.brick_double_slab;
-		int indexColoredDouble = ModBlocks.concrete_colored_double_slabs.indexOf(b);
-		if(indexColoredDouble >= 0) return (BlockSlab)b;
-		int indexColoredSingle = ModBlocks.concrete_colored_slabs.indexOf(b);
-		if(indexColoredSingle >= 0) return (BlockSlab)ModBlocks.concrete_colored_double_slabs.get(indexColoredSingle);
-		int indexColoredExtDouble = ModBlocks.concrete_colored_ext_double_slabs.indexOf(b);
-		if(indexColoredExtDouble >= 0) return (BlockSlab)b;
-		int indexColoredExtSingle = ModBlocks.concrete_colored_ext_slabs.indexOf(b);
-		if(indexColoredExtSingle >= 0) return (BlockSlab)ModBlocks.concrete_colored_ext_double_slabs.get(indexColoredExtSingle);
+		if (GeneralConfig.enableColoredStairsSlabs) {
+			int indexColoredDouble = ModBlocks.concrete_colored_double_slabs.indexOf(b);
+			if(indexColoredDouble >= 0) return (BlockSlab)b;
+			int indexColoredSingle = ModBlocks.concrete_colored_slabs.indexOf(b);
+			if(indexColoredSingle >= 0) return (BlockSlab)ModBlocks.concrete_colored_double_slabs.get(indexColoredSingle);
+			int indexColoredExtDouble = ModBlocks.concrete_colored_ext_double_slabs.indexOf(b);
+			if(indexColoredExtDouble >= 0) return (BlockSlab)b;
+			int indexColoredExtSingle = ModBlocks.concrete_colored_ext_slabs.indexOf(b);
+			if(indexColoredExtSingle >= 0) return (BlockSlab)ModBlocks.concrete_colored_ext_double_slabs.get(indexColoredExtSingle);
+		}
 		return null;
 	}
 }
