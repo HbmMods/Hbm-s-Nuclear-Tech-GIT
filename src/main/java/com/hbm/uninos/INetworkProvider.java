27bd64eafc3cd7b23f1148e90e50bcebddb23d20
Boblet <hbmmods@gmail.com>
2025-02-04 16:54:02 +0100

    a botched nosejob

---
+++ src/main/java/com/hbm/uninos/INetworkProvider.java
@@ -0,0 +1,5 @@
+package com.hbm.uninos;
+
+public interface INetworkProvider {
+
+}
---
+++ src/main/java/com/hbm/uninos/UniNodespace.java
@@ -0,0 +1,8 @@
+package com.hbm.uninos;
+
+import java.util.HashMap;
+
+public class UniNodespace {
+
+	public static HashMap<INetworkProvider, GenNodespace> nodespaces = new HashMap();
+}
