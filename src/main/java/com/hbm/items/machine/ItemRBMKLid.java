bb84ebe2092cf16fa080bfffb73ce18b09c19bb6
George Paton <georgewpaton@me.com>
2025-02-26 15:38:31 +1100

    Fix #1918 - make sure node caching is per world, remove nodes from cache correctly, and prevent unloading dimensions immediately ceasing all reactivity. Also fixes rod information not updating unless the GUI is open

--- src/main/java/com/hbm/items/machine/ItemRBMKLid.java
+++ src/main/java/com/hbm/items/machine/ItemRBMKLid.java
@@ -20,46 +20,46 @@ public class ItemRBMKLid extends Item {
 
 	@Override
 	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float fx, float fy, float fz) {
-		
+
 		Block b = world.getBlock(x, y, z);
-		
+
 		if(b instanceof RBMKBase) {
 			RBMKBase rbmk = (RBMKBase) b;
-			
+
 			int[] pos = rbmk.findCore(world, x, y, z);
-			
+
 			if(pos == null)
 				return false;
-			
+
 			TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
-			
+
 			if(!(te instanceof TileEntityRBMKBase))
 				return false;
-			
+
 			TileEntityRBMKBase tile = (TileEntityRBMKBase) te;
-			
+
 			if(tile.hasLid())
 				return false;
 
-			RBMKNeutronNode node = (RBMKNeutronNode) NeutronNodeWorld.getNode(new BlockPos(te));
-			if (node != null)
+			RBMKNeutronNode node = (RBMKNeutronNode) NeutronNodeWorld.getNode(world, new BlockPos(te));
+			if(node != null)
 				node.addLid();
 
 			int meta = RBMKBase.DIR_NORMAL_LID.ordinal();
-			
+
 			if(this == ModItems.rbmk_lid_glass) {
 				meta = RBMKBase.DIR_GLASS_LID.ordinal();
 				world.playSoundEffect(x + 0.5, y + 0.5, z + 0.5, Blocks.glass.stepSound.func_150496_b(), (Blocks.glass.stepSound.getVolume() + 1.0F) / 2.0F, Blocks.glass.stepSound.getPitch() * 0.8F);
 			} else {
 				world.playSoundEffect(x + 0.5, y + 0.5, z + 0.5, ModBlocks.concrete_smooth.stepSound.func_150496_b(), (ModBlocks.concrete_smooth.stepSound.getVolume() + 1.0F) / 2.0F, ModBlocks.concrete_smooth.stepSound.getPitch() * 0.8F);
 			}
-			
+
 			world.setBlockMetadataWithNotify(pos[0], pos[1], pos[2], meta + RBMKBase.offset, 3);
 			stack.stackSize--;
-			
+
 			return true;
 		}
-		
+
 		return false;
 	}
 }
