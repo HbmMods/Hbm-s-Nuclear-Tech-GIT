f0679b04cc0f94b904a514c96d71cf40a323a615
PewPewCricket <pewpewcricket@proton.me>
2025-07-24 13:30:59 -0500

    fix FCP oc compat

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineTurbine.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineTurbine.java
@@ -236,7 +236,7 @@ public class TileEntityMachineTurbine extends TileEntityLoadedBase implements IS
 			}
 		}
 		nbt.setTag("items", list);
-		
+
 		if (customName != null) {
 			nbt.setString("name", customName);
 		}
@@ -280,7 +280,7 @@ public class TileEntityMachineTurbine extends TileEntityLoadedBase implements IS
 				this.tryProvide(worldObj, xCoord + dir.offsetX, yCoord + dir.offsetY, zCoord + dir.offsetZ, dir);
 
 			tanks[0].setType(0, 1, slots);
-			tanks[0].loadTank(2, 3, slots);
+			tanks[0].loadTank(2,  3, slots);
 			power = Library.chargeItemsFromTE(slots, 4, power, maxPower);
 
 			FluidType in = tanks[0].getTankType();
