b4b471e5c3f5ccf8b01a3f6a5f97e89976c9d190
FOlkvangrField <nukekonglong@hotmail.com>
2025-08-20 21:46:59 +0800

    Fix

--- src/main/java/com/hbm/qmaw/GuiQMAW.java
+++ src/main/java/com/hbm/qmaw/GuiQMAW.java
@@ -23,7 +23,7 @@ import net.minecraft.util.ResourceLocation;
 public class GuiQMAW extends GuiScreen {
 
 	protected static final ResourceLocation texture = new ResourceLocation(RefStrings.MODID + ":textures/gui/gui_wiki.png");
-	
+
 	public String title;
 	public String qmawID;
 	public ItemStack icon;
@@ -31,51 +31,51 @@ public class GuiQMAW extends GuiScreen {
 	/** History for returning via button */
 	public List<String> back = new ArrayList();
 	public List<String> forward = new ArrayList();
-	
+
 	protected int xSize = 340;
 	protected int ySize = 224;
 	protected int guiLeft;
 	protected int guiTop;
-	
+
 	protected boolean isDragging = false;
 	protected int scrollProgress = 0;
 	protected int lastClickX = 0;
 	protected int lastClickY = 0;
-	
+
 	public static final String EN_US = "en_US";
-	
+
 	public GuiQMAW(QuickManualAndWiki qmaw) {
 		qmawID = qmaw.name;
 		parseQMAW(qmaw);
 	}
-	
+
 	protected void parseQMAW(QuickManualAndWiki qmaw) {
 		LanguageManager lang = Minecraft.getMinecraft().getLanguageManager();
-		
-		this.title = qmaw.title.get(lang.getCurrentLanguage());
+
+		this.title = qmaw.title.get(lang.getCurrentLanguage().getLanguageCode());
 		if(title == null) this.title = qmaw.title.get(EN_US);
 		if(title == null) this.title = "Missing Localization!";
-		
+
 		this.icon = qmaw.icon;
-		
-		String toParse = qmaw.contents.get(lang.getCurrentLanguage());
+
+		String toParse = qmaw.contents.get(lang.getCurrentLanguage().getLanguageCode());
 		if(toParse == null) toParse = qmaw.contents.get(EN_US);
 		if(toParse == null) toParse = "Missing Localization!";
 		toParse = "" + toParse; // strings are reference types, no?
-		
+
 		int maxLineLength = xSize - 29;
 		String prevToParse = "" + toParse;
 		int maxIterations = 1000;
 		int currentLineWidth = 0;
-		
+
 		while(!toParse.isEmpty() && maxIterations > 0) {
 			if(this.lines.isEmpty()) this.lines.add(new ArrayList());
 			List<ManualElement> currentLine = this.lines.get(this.lines.size() - 1);
-			
+
 			toParse = toParse.trim();
-			
+
 			maxIterations--;
-			
+
 			if(toParse.startsWith("<br>")) {
 				toParse = toParse.substring(4);
 				currentLine = new ArrayList();
@@ -83,25 +83,25 @@ public class GuiQMAW extends GuiScreen {
 				currentLineWidth = 0;
 				continue;
 			}
-			
+
 			// handle links
 			if(toParse.startsWith("[[")) {
 				int end = toParse.indexOf("]]");
 				if(end != -1) {
 					String link = toParse.substring(2, end);
 					toParse = toParse.substring(end + 2);
-					
+
 					int pipe = link.indexOf("|");
 					QComponentLink linkComponent;
-					
+
 					String suffix = toParse.startsWith(" ") ? " " : "";
-					
+
 					if(pipe == -1) {
 						linkComponent = new QComponentLink(link, link + suffix);
 					} else {
 						linkComponent = new QComponentLink(link.substring(pipe + 1, link.length()), link.substring(0, pipe) + suffix);
 					}
-					
+
 					// append to current line
 					int width = linkComponent.getWidth();
 					if(width + currentLineWidth <= maxLineLength) {
@@ -119,21 +119,21 @@ public class GuiQMAW extends GuiScreen {
 					continue;
 				}
 			}
-			
+
 			// handle standard text
 			int delimit = toParse.length();
-			
+
 			int spaceIndex = toParse.indexOf(" ");
 			if(spaceIndex != -1) delimit = Math.min(delimit, spaceIndex);
 			int linkIndex = toParse.indexOf("[[");
 			if(linkIndex != -1) delimit = Math.min(delimit, linkIndex);
 			int brIndex = toParse.indexOf("<br>");
 			if(brIndex != -1) delimit = Math.min(delimit, brIndex);
-			
+
 			if(delimit > 0) {
 				QComponentText textComponent = new QComponentText(toParse.substring(0, delimit) + (spaceIndex == delimit ? " " : ""));
 				toParse = toParse.substring(delimit);
-				
+
 				// append to current line
 				int width = textComponent.getWidth();
 				if(width + currentLineWidth <= maxLineLength) {
@@ -150,7 +150,7 @@ public class GuiQMAW extends GuiScreen {
 				prevToParse = "" + toParse;
 				continue;
 			}
-			
+
 			if(toParse.equals(prevToParse)) break;
 			prevToParse = "" + toParse;
 		}
@@ -166,7 +166,7 @@ public class GuiQMAW extends GuiScreen {
 	@Override
 	protected void mouseClicked(int x, int y, int key) {
 		super.mouseClicked(x, y, key);
-		
+
 		if(key == 0) {
 			this.lastClickX = x;
 			this.lastClickY = y;
@@ -175,10 +175,10 @@ public class GuiQMAW extends GuiScreen {
 		if(guiLeft + 3 <= x && guiLeft + 3 + 18 > x && guiTop + 3 < y && guiTop + 3 + 18 >= y) back();
 		if(guiLeft + 21 <= x && guiLeft + 21 + 18 > x && guiTop + 3 < y && guiTop + 3 + 18 >= y) forward();
 	}
-	
+
 	public void back() {
 		if(this.back.isEmpty()) return;
-		
+
 		String prev = back.get(back.size() - 1);
 
 		QuickManualAndWiki qmaw = QMAWLoader.qmaw.get(prev);
@@ -192,10 +192,10 @@ public class GuiQMAW extends GuiScreen {
 			FMLCommonHandler.instance().showGuiScreen(screen);
 		}
 	}
-	
+
 	public void forward() {
 		if(this.forward.isEmpty()) return;
-		
+
 		String next = forward.get(forward.size() - 1);
 
 		QuickManualAndWiki qmaw = QMAWLoader.qmaw.get(next);
@@ -209,7 +209,7 @@ public class GuiQMAW extends GuiScreen {
 			FMLCommonHandler.instance().showGuiScreen(screen);
 		}
 	}
-	
+
 	public int getSliderPosition() {
 		double progress = (double) scrollProgress / (double) (lines.size() - 1);
 		return 25 + (int) (progress * 180);
@@ -217,38 +217,38 @@ public class GuiQMAW extends GuiScreen {
 
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float f) {
-		
+
 		if(Mouse.isButtonDown(0) && guiLeft + xSize - 15 <= mouseX && guiLeft + xSize - 15 + 12 > mouseX && guiTop + 25 < mouseY && guiTop + 25 + 191 >= mouseY) {
 			isDragging = true;
 		}
-		
+
 		if(!Mouse.isButtonDown(0)) isDragging = false;
-		
+
 		if(isDragging) {
 			int min = guiTop + 25 + 8;
 			int max = guiTop + 25 + 191 - 8;
 			int span = max - min;
-			
+
 			double progress = MathHelper.clamp_double((double) (mouseY - min) / span, 0D, 1D);
 			this.scrollProgress = MathHelper.clamp_int((int) Math.round((lines.size() - 1) * progress), 0, lines.size() - 1);
 		}
-		
+
 		handleScroll();
 
 		//this.drawRect(0, 0, this.width, this.height, 0x80919191);
 		this.drawRect(0, 0, this.width, this.height, 0xe0000000);
-		
+
 		this.drawGuiContainerBackgroundLayer(f, mouseX, mouseY);
 		GL11.glDisable(GL11.GL_LIGHTING);
 		this.drawGuiContainerForegroundLayer(mouseX, mouseY);
 		GL11.glEnable(GL11.GL_LIGHTING);
-		
+
 		this.lastClickX = 0;
 		this.lastClickY = 0;
 	}
-	
+
 	protected void handleScroll() {
-		
+
 		if(!Mouse.isButtonDown(0) && !Mouse.isButtonDown(1) && Mouse.next()) {
 			int scroll = Mouse.getEventDWheel();
 			if(scroll > 0 && this.scrollProgress > 0) this.scrollProgress--;
@@ -257,10 +257,10 @@ public class GuiQMAW extends GuiScreen {
 	}
 
 	private void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		
+
 		int x = 43;
 		int y = 4;
-		
+
 		if(this.icon != null) {
 			GL11.glPushMatrix();
 			GL11.glEnable(GL11.GL_DEPTH_TEST);
@@ -273,13 +273,13 @@ public class GuiQMAW extends GuiScreen {
 			RenderHelper.disableStandardItemLighting();
 			GL11.glDisable(GL11.GL_DEPTH_TEST);
 			GL11.glPopMatrix();
-			
+
 			x += 18;
 			y += (16 - this.fontRendererObj.FONT_HEIGHT) / 2;
 		}
-		
+
 		y += 1;
-		
+
 		this.fontRendererObj.drawString(title, guiLeft + x, guiTop + y, 0xFFFFFF);
 	}
 
@@ -292,30 +292,30 @@ public class GuiQMAW extends GuiScreen {
 
 		if(!back.isEmpty()) drawTexturedModalRect(guiLeft + 3, guiTop + 3, 204, 0, 18, 18);
 		if(!forward.isEmpty()) drawTexturedModalRect(guiLeft + 21, guiTop + 3, 222, 0, 18, 18);
-		
+
 		// scroll bar
 		drawTexturedModalRect(guiLeft +  xSize - 15, guiTop + getSliderPosition(), 192, 0, 12, 16);
-		
+
 		int x = guiLeft + 7;
 		int y = guiTop + 30;
 		int lineNum = 0;
-		
+
 		for(List<ManualElement> line : lines) {
 			lineNum++;
-			
+
 			if(lineNum <= this.scrollProgress) continue;
-			
+
 			int maxHeight = 0;
 			int inset = 0;
-			
+
 			for(ManualElement element : line) {
 				maxHeight = Math.max(maxHeight, element.getHeight());
 			}
-			
+
 			if(y + maxHeight > guiTop + 219) break;
-			
+
 			if(line.isEmpty()) y += this.fontRendererObj.FONT_HEIGHT;
-			
+
 			for(ManualElement element : line) {
 				int elementX = x + inset;
 				int elementY = y + (maxHeight - element.getHeight()) / 2;
@@ -325,7 +325,7 @@ public class GuiQMAW extends GuiScreen {
 					element.onClick(this);
 				inset += element.getWidth();
 			}
-			
+
 			y += maxHeight + 2;
 		}
 	}
@@ -335,7 +335,7 @@ public class GuiQMAW extends GuiScreen {
 
 		if(keyCode == Keyboard.KEY_LEFT) back();
 		if(keyCode == Keyboard.KEY_RIGHT) forward();
-		
+
 		if(keyCode == 1 || keyCode == this.mc.gameSettings.keyBindInventory.getKeyCode()) {
 			this.mc.displayGuiScreen((GuiScreen) null);
 			this.mc.setIngameFocus();
