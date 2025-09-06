8a029b94bc45f6c1cfaf2df77b3ccdb09de1eaf6
Bufka2011 <bufka.bulanov@gmail.com>
2025-09-05 21:11:07 -0600

    polaroid

--- src/main/java/com/hbm/items/special/ItemPolaroid.java
+++ src/main/java/com/hbm/items/special/ItemPolaroid.java
@@ -24,68 +24,14 @@ public class ItemPolaroid extends Item {
     }
 
 	@Override
-	public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean bool)
-	{
-		list.add(I18nUtil.resolveKey("item.polaroid.desc."));
-		list.add("");
-		switch(MainRegistry.polaroidID) {
-			case 1:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.1"));
-				break;
-			case 2:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.2"));
-				break;
-			case 3:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.3"));
-				break;
-			case 4:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.4"));
-				break;
-			case 5:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.5"));
-				break;
-			case 6:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.6"));
-				break;
-			case 7:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.7"));
-				break;
-			case 8:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.8"));
-				break;
-			case 9:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.9"));
-				break;
-			case 10:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.10"));
-				break;
-			case 11:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.11"));
-				break;
-			case 12:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.12"));
-				break;
-			case 13:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.13"));
-				break;
-			case 14:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.14"));
-				break;
-			case 15:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.15"));
-				break;
-			case 16:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.16"));
-				break;
-			case 17:
-				list.add(I18nUtil.resolveKey("item.polaroid.desc.17"));
-				break;
-			case 18:
-				String[] lines = I18nUtil.resolveKeyArray("item.polaroid.desc.18");
-				for (String line : lines) {
-					list.add(line);
-				}
-				break;
+	public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean advanced) {
+		tooltip.add(I18nUtil.resolveKey("item.polaroid.desc."));
+
+		String baseKey = this.getUnlocalizedName(stack) + ".desc";
+		String[] extraLines = I18nUtil.resolveKeyArray(baseKey);
+
+		for (String line : extraLines) {
+			tooltip.add(line);
 		}
 	}
 }
