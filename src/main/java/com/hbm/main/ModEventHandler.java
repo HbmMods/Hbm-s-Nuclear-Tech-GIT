dadd7d144fd52802dc023f4c1061cdd235070241
Bufka2011 <bufka.bulanov@gmail.com>
2025-09-05 20:40:03 -0600

    Fixed

--- src/main/java/com/hbm/main/ModEventHandler.java
+++ src/main/java/com/hbm/main/ModEventHandler.java
@@ -133,27 +133,26 @@ public class ModEventHandler {
 		if(!event.player.worldObj.isRemote) {
 
 			if(GeneralConfig.enableMOTD) {
-				event.player.addChatMessage(new ChatComponentText(I18nUtil.format("info.world_loaded", RefStrings.VERSION)));
+				event.player.addChatMessage(new ChatComponentTranslation("info.world_loaded", RefStrings.VERSION));
 
 				if(HTTPHandler.newVersion) {
 					event.player.addChatMessage(
-							new ChatComponentText("New version " + HTTPHandler.versionNumber + " is available! Click ")
+						new ChatComponentTranslation("info.new_version_available", HTTPHandler.versionNumber)
 							.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.YELLOW))
 							.appendSibling(new ChatComponentText("[here]")
-									.setChatStyle(new ChatStyle()
-										.setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/HbmMods/Hbm-s-Nuclear-Tech-GIT/releases"))
-										.setUnderlined(true)
-										.setColor(EnumChatFormatting.RED)
-									)
+								.setChatStyle(new ChatStyle()
+									.setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/HbmMods/Hbm-s-Nuclear-Tech-GIT/releases"))
+									.setUnderlined(true)
+									.setColor(EnumChatFormatting.RED)
 								)
-							.appendSibling(new ChatComponentText(" to download!").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.YELLOW)))
-							);
+							)
+							.appendSibling(new ChatComponentTranslation("info.download").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.YELLOW)))
+					);
 				}
 			}
 
 			if(MobConfig.enableDucks && event.player instanceof EntityPlayerMP && !event.player.getEntityData().getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG).getBoolean("hasDucked"))
 				PacketDispatcher.wrapper.sendTo(new PlayerInformPacket("Press O to Duck!", MainRegistry.proxy.ID_DUCK, 30_000), (EntityPlayerMP) event.player);
-
 
 			if(GeneralConfig.enableGuideBook) {
 				HbmPlayerProps props = HbmPlayerProps.getData(event.player);
@@ -173,7 +172,7 @@ public class ModEventHandler {
 				boolean hasSent = false;
 
 				for(SerializableRecipe recipe : SerializableRecipe.recipeHandlers) {
-					File recFile = new File(recDir.getAbsolutePath() + File.separatorChar + recipe.getFileName());
+					File recFile = new File(recDir.getAbsolutePath() + File.separatorChar + "hbmRecipes" + File.separatorChar + recipe.getFileName());
 					if(recFile.exists() && recFile.isFile()) {
 						MainRegistry.logger.info("Sending recipe file: " + recFile.getName());
 						PacketDispatcher.wrapper.sendTo(new SerializableRecipePacket(recFile), (EntityPlayerMP) event.player);
