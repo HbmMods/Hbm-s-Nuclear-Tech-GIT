09dd5e37b53d5c77debb92086a81452039946212
George Paton <georgewpaton@me.com>
2025-10-31 14:33:09 +1100

    nodespace foundry channels! (also fix arc furnace only accepting one stack from IO port if it triggers it to close)

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineArcFurnaceLarge.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineArcFurnaceLarge.java
@@ -113,8 +113,8 @@ public class TileEntityMachineArcFurnaceLarge extends TileEntityMachineBase impl
 			this.isProgressing = false;
 
 			for(DirPos pos : getConPos()) this.trySubscribe(worldObj, pos.getX(), pos.getY(), pos.getZ(), pos.getDir());
-			
-			if(lid == 1) loadIngredients();
+
+			if(lid > 0) loadIngredients();
 
 			if(power > 0) {
 
@@ -269,17 +269,17 @@ public class TileEntityMachineArcFurnaceLarge extends TileEntityMachineBase impl
 			}
 		}
 	}
-	
+
 	/** Moves items from the input queue to the main grid */
 	public void loadIngredients() {
-		
+
 		boolean markDirty = false;
-		
+
 		for(int q /* queue */ = 25; q < 30; q++) {
 			if(slots[q] == null) continue;
 			ArcFurnaceRecipe recipe = ArcFurnaceRecipes.getOutput(slots[q], this.liquidMode);
 			if(recipe == null) continue;
-			
+
 			// add to existing stacks
 			for(int i /* ingredient */ = 5; i < 25; i++) {
 				if(slots[i] == null) continue;
@@ -293,7 +293,7 @@ public class TileEntityMachineArcFurnaceLarge extends TileEntityMachineBase impl
 				}
 				if(slots[q] == null) break;
 			}
-			
+
 			// add to empty slot
 			if(slots[q] != null) for(int i /* ingredient */ = 5; i < 25; i++) {
 				if(slots[i] != null) continue;
@@ -306,7 +306,7 @@ public class TileEntityMachineArcFurnaceLarge extends TileEntityMachineBase impl
 				if(slots[q] == null) break;
 			}
 		}
-		
+
 		if(markDirty) this.markDirty();
 	}
 
---
+++ src/main/java/com/hbm/uninos/networkproviders/FoundryNetwork.java
@@ -0,0 +1,10 @@
+package com.hbm.uninos.networkproviders;
+
+import com.hbm.uninos.NodeNet;
+
+public class FoundryNetwork extends NodeNet {
+
+	@Override
+	public void update() { }
+
+}
