bce98561d37ec859ac75f3508df77499cc83246a
WolfEclipses <dragonwolfgaming97@gmail.com>
2025-12-09 08:31:52 -0500

    Added back old ones, changed lang to OLD for them

--- src/main/java/com/hbm/blocks/ModBlocks.java
+++ src/main/java/com/hbm/blocks/ModBlocks.java
@@ -1189,6 +1189,10 @@ public class ModBlocks {
 	public static Block gas_explosive;
 	public static Block vacuum;
 
+	public static Block absorber;
+	public static Block absorber_red;
+	public static Block absorber_green;
+	public static Block absorber_pink;
 	public static BlockAbsorber rad_absorber;
 	public static Block decon;
 
@@ -2359,6 +2363,13 @@ public class ModBlocks {
 		gas_explosive = new BlockGasExplosive().setBlockName("gas_explosive").setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":gas_explosive");
 		vacuum = new BlockVacuum().setBlockName("vacuum").setResistance(1000000F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":vacuum");
 
+		// OLD ABSORBERS
+		absorber = new BlockAbsorberOLD(Material.iron, 2.5F).setBlockName("absorber").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":absorber");
+		absorber_red = new BlockAbsorberOLD(Material.iron, 10F).setBlockName("absorber_red").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":absorber_red");
+		absorber_green = new BlockAbsorberOLD(Material.iron, 100F).setBlockName("absorber_green").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":absorber_green");
+		absorber_pink = new BlockAbsorberOLD(Material.iron, 10000F).setBlockName("absorber_pink").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":absorber_pink");
+
+		//NEW ABSORBER
 		rad_absorber = new BlockAbsorber(Material.iron).setBlockName("rad_absorber").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab);
 		decon = new BlockDecon(Material.iron).setBlockName("decon").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":decon_side");
 
@@ -3399,7 +3410,11 @@ public class ModBlocks {
 		GameRegistry.registerBlock(struct_icf_core, struct_icf_core.getUnlocalizedName());
 
 		//Absorbers
-		GameRegistry.registerBlock(rad_absorber, ItemBlockAbsorber.class, "rad_absorber");
+		GameRegistry.registerBlock(absorber, absorber.getUnlocalizedName());
+		GameRegistry.registerBlock(absorber_red, absorber_red.getUnlocalizedName());
+		GameRegistry.registerBlock(absorber_green, absorber_green.getUnlocalizedName());
+		GameRegistry.registerBlock(absorber_pink, absorber_pink.getUnlocalizedName());
+		GameRegistry.registerBlock(rad_absorber, ItemBlockAbsorber.class, rad_absorber.getUnlocalizedName());
 		GameRegistry.registerBlock(decon, decon.getUnlocalizedName());
 
 		//Solar Tower Blocks
