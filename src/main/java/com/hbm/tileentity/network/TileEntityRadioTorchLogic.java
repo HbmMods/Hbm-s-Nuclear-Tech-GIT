2d0a0986c1e2a235e28551444daf07468222ad73
gjhhjb vhjgv <ainur20082003@gmail.com>
2026-08-18 14:23:32 +0300

    Fix polling RTTY receivers on empty channels

--- src/main/java/com/hbm/tileentity/network/TileEntityRadioTorchLogic.java
+++ src/main/java/com/hbm/tileentity/network/TileEntityRadioTorchLogic.java
@@ -41,9 +41,8 @@ public class TileEntityRadioTorchLogic extends TileEntityLoadedBase implements I
 
 		if(!worldObj.isRemote) {
 
-			if(!this.channel.isEmpty()) {
-
-				RTTYChannel chan = RTTYSystem.listen(worldObj, this.channel);
+			if(!this.channel.isEmpty() || this.polling) {
+				RTTYChannel chan = this.channel.isEmpty() ? null : RTTYSystem.listen(worldObj, this.channel);
 
 				if(chan != null && (this.polling || (chan.timeStamp > this.lastUpdate - 1 && chan.timeStamp != -1))) { // if we're either polling or a new message has come in
 					String msg = "" + chan.signal;
@@ -78,6 +77,11 @@ public class TileEntityRadioTorchLogic extends TileEntityLoadedBase implements I
 						worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, this.getBlockType());
 						this.markDirty();
 					}
+				} else if(this.polling && this.lastState != 0) {
+					this.lastState = 0;
+					worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+					worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, this.getBlockType());
+					this.markDirty();
 				}
 			}
 
