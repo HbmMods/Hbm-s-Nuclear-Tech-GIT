ebccd5b84fe7107e77314ee446411801bbacfc8b
gjhhjb vhjgv <ainur20082003@gmail.com>
2026-08-25 16:49:31 +0300

    Fix tape drive shift-click item loss

--- src/main/java/com/hbm/inventory/container/ContainerTapeDrive.java
+++ src/main/java/com/hbm/inventory/container/ContainerTapeDrive.java
@@ -1,7 +1,12 @@
 package com.hbm.inventory.container;
 
+import com.hbm.util.InventoryUtil;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerTapeDrive extends ContainerBase {
 
@@ -11,5 +16,32 @@ public class ContainerTapeDrive extends ContainerBase {
 		this.addSlots(tedf, 0, 35, 27, 2, 6);
 		
 		this.playerInv(invPlayer, 8, 104);
+	}
+
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int index) {
+		ItemStack slotOriginal = null;
+		Slot slot = (Slot) this.inventorySlots.get(index);
+
+		if(slot != null && slot.getHasStack()) {
+			ItemStack slotStack = slot.getStack();
+			slotOriginal = slotStack.copy();
+			if(index <= tile.getSizeInventory() - 1) {
+				if(!InventoryUtil.mergeItemStack(this.inventorySlots, slotStack, tile.getSizeInventory(), this.inventorySlots.size(), true)) {
+					return null;
+				}
+			} else if(!InventoryUtil.mergeItemStack(this.inventorySlots, slotStack, 0, tile.getSizeInventory(), false)) {
+				return null;
+			}
+
+			if(slotStack.stackSize == 0) {
+				slot.putStack(null);
+			} else {
+				slot.onSlotChanged();
+			}
+
+			slot.onPickupFromSlot(player, slotStack);
+		}
+		return slotOriginal;
 	}
 }
