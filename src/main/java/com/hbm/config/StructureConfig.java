273071ca14b93f7dd22ae256c31c5011819b280f
CreeperTyp89 <spamyspamton1997@gmail.com>
2026-09-16 01:59:41 +0200

    hopefully this shit works now

--- src/main/java/com/hbm/config/StructureConfig.java
+++ src/main/java/com/hbm/config/StructureConfig.java
@@ -125,7 +125,7 @@ public class StructureConfig {
 		waterPumpSpawnWeight = CommonConfig.createConfigInt(config, CATEGORY_STRUCTURES, "5.42_waterPumpSpawnWeight", "Spawn weight for water pump structure.", 15);
 		deadDishSmallSpawnWeight = CommonConfig.createConfigInt(config, CATEGORY_STRUCTURES, "5.43_deadDishSmallSpawnWeight", "Spawn weight for dead dish small structure.", 15);
 		falloutShelterHouseSpawnWeight = CommonConfig.createConfigInt(config, CATEGORY_STRUCTURES, "5.44_falloutShelterHouseSpawnWeight", "Spawn weight for fallout shelter house.", 15);
-		abandonedArcticStationSpawnWeight = CommonConfig.createConfigInt(config, CATEGORY_STRUCTURES, "5.44_abandonedArcticStationSpawnWeight", "Spawn weight for abandoned arctic station.", 5);
+		abandonedArcticStationSpawnWeight = CommonConfig.createConfigInt(config, CATEGORY_STRUCTURES, "5.45_abandonedArcticStationSpawnWeight", "Spawn weight for abandoned arctic station.", 5);
 
 
 		structureMinChunks = CommonConfig.setDef(structureMinChunks, 4);
