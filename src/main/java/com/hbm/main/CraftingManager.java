211da555a815d03db5393b79cd9df0338a104bfd
CreeperTyp89 <spamyspamton1997@gmail.com>
2026-09-15 17:10:33 +0200

    build a dinner

--- src/main/java/com/hbm/main/CraftingManager.java
+++ src/main/java/com/hbm/main/CraftingManager.java
@@ -380,6 +380,8 @@ public class CraftingManager {
 		addRecipeAuto(new ItemStack(ModBlocks.tile_lab, 4), new Object[] { "CBC", "CBC", "CBC", 'C', Items.brick, 'B', ASBESTOS.ingot() });
 		addRecipeAuto(new ItemStack(ModBlocks.tile_lab_cracked, 6), new Object[] { " C " , "C C", " C ", 'C', ModBlocks.tile_lab });
 		addRecipeAuto(new ItemStack(ModBlocks.tile_lab_broken, 6), new Object[] { " C " , "C C", " C ", 'C', ModBlocks.tile_lab_cracked });
+		addRecipeAuto(new ItemStack(ModBlocks.tile_checkerboard, 4), new Object[] { "CBC", "BCB", "CBC", 'C', Items.clay_ball, 'B', new ItemStack(Items.dye, 1, 0) });
+		addRecipeAuto(new ItemStack(ModBlocks.tile_checkerboard_small, 8), new Object[] { "CC", "CC", 'C', ModBlocks.tile_checkerboard });
 		addShapelessAuto(new ItemStack(ModBlocks.asphalt_light, 1), new Object[] { ModBlocks.asphalt, Items.glowstone_dust });
 		addShapelessAuto(new ItemStack(ModBlocks.asphalt, 1), new Object[] { ModBlocks.asphalt_light });
 
@@ -782,7 +784,6 @@ public class CraftingManager {
 		addRecipeAuto(new ItemStack(ModBlocks.rbmk_terminal, 1), new Object[] { "R ", "CD", "B ", 'R', ModBlocks.radio_torch_sender, 'B', ModBlocks.rbmk_display_blank, 'C', DictFrame.fromOne(ModItems.circuit, EnumCircuitType.ANALOG), 'D', ModItems.crt_display });
 
 		addRecipeAuto(new ItemStack(ModItems.rtty_pager, 1), new Object[] { "R", "C", "S", 'R', ModBlocks.radio_torch_receiver, 'C', DictFrame.fromOne(ModItems.circuit, EnumCircuitType.BASIC), 'S', STEEL.plate() });
-		addRecipeAuto(new ItemStack(ModItems.rtty_designator, 1), new Object[] {"R", "C", "T", 'R', ModBlocks.radio_torch_reader, 'C', DictFrame.fromOne(ModItems.circuit, EnumCircuitType.ANALOG), 'T', ModItems.rangefinder});
 
 		addRecipeAuto(new ItemStack(ModBlocks.deco_rbmk, 8), new Object[] { "R", 'R', ModBlocks.rbmk_blank });
 		addRecipeAuto(new ItemStack(ModBlocks.deco_rbmk_smooth, 1), new Object[] { "R", 'R', ModBlocks.deco_rbmk });
