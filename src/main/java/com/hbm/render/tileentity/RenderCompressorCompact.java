df0f1b5a7d81225f211fdd9d386c916f6e5435e6
LegendaryDoge30 <legendarydoge9@gmail.com>
2026-09-22 19:50:50 +0200

    wow
    wow (description)

--- src/main/java/com/hbm/render/tileentity/RenderCompressorCompact.java
+++ src/main/java/com/hbm/render/tileentity/RenderCompressorCompact.java
@@ -29,28 +29,9 @@ public class RenderCompressorCompact extends TileEntitySpecialRenderer implement
 		case 5: GL11.glRotatef(0, 0F, 1F, 0F); break;
 		}
 		
-		TileEntityMachineCompressorCompact compressor = (TileEntityMachineCompressorCompact) tileEntity;
-		
 		GL11.glShadeModel(GL11.GL_SMOOTH);
 		bindTexture(ResourceManager.compressor_compact_tex);
-		ResourceManager.condenser.renderPart("Condenser");
-
-		float rot = compressor.prevFanSpin + (compressor.fanSpin - compressor.prevFanSpin) * f;
-		
-		GL11.glPushMatrix();
-		GL11.glTranslated(0,1.5, 0);
-		GL11.glRotatef(rot, 1, 0, 0);
-		GL11.glTranslated(0, -1.5, 0);
-		ResourceManager.condenser.renderPart("Fan1");
-		GL11.glPopMatrix();
-		
-		GL11.glPushMatrix();
-		GL11.glTranslated(0,1.5, 0);
-		GL11.glRotatef(rot, -1, 0, 0);
-		GL11.glTranslated(0, -1.5, 0);
-		ResourceManager.condenser.renderPart("Fan2");
-		GL11.glPopMatrix();
-		
+		ResourceManager.compressor_compact.renderAll();
 		GL11.glShadeModel(GL11.GL_FLAT);
 		
 		GL11.glEnable(GL11.GL_CULL_FACE);
@@ -73,7 +54,7 @@ public class RenderCompressorCompact extends TileEntitySpecialRenderer implement
 				GL11.glScaled(0.75, 0.75, 0.75);
 				GL11.glTranslated(0.5, 0, 0);
 				GL11.glShadeModel(GL11.GL_SMOOTH);
-				bindTexture(ResourceManager.compressor_compact_tex); ResourceManager.condenser.renderAll();
+				bindTexture(ResourceManager.compressor_compact_tex); ResourceManager.compressor_compact.renderAll();
 				GL11.glShadeModel(GL11.GL_FLAT);
 			}
 		};
